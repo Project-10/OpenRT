2889e2dc9423a2ea148f686fe9d9c2b3ccd67fe0
Sergey Kosov <sergey.kosov@project-10.de>
2019-11-06 23:46:17 +0100

    Windows compatibility

--- modules/core/BoundingBox.h
+++ modules/core/BoundingBox.h
@@ -11,36 +11,36 @@ namespace rt {
 	class CBoundingBox
 	{
 	public:
-		CBoundingBox(void) = default;
-		~CBoundingBox(void)= default;
+		DllExport CBoundingBox(void) = default;
+		DllExport ~CBoundingBox(void)= default;
 		
 		/**
 		 * @brief Resets the bounding box
 		 * @details This function resets the member variables \b m_min and \b m_max
 		 */
-		void clear(void);
+		DllExport void clear(void);
 		/**
 		 * @brief Extends the bounding box to contain point \b a
 		 * @param a A point
 		 */
-		void extend(Vec3f a);
+		DllExport void extend(Vec3f a);
 		/**
 		 * @brief Extends the bounding box to contain bounding box \b box
 		 * @param box The second bounding box
 		 */
-		void extend(const CBoundingBox& box);
+		DllExport void extend(const CBoundingBox& box);
 		/**
 		 * @brief Checks if the current bounding box overlaps with the argument bounding box \b box
 		 * @param box The secind bounding box to be checked with
 		 */
-		bool overlaps(const CBoundingBox& box);
+		DllExport bool overlaps(const CBoundingBox& box);
 		/**
 		 * @brief Clips the ray with the bounding box
 		 * @param[in] ray The ray
 		 * @param[in,out] t0 The distance from ray origin at which the ray enters the bounding box
 		 * @param[in,out] t1 The distance from ray origin at which the ray leaves the bounding box
 		 */
-		void clip(const Ray& ray, float& t0, float& t1);
+		DllExport void clip(const Ray& ray, float& t0, float& t1);
 		
 		
 	public:
