ffa52f755e66f8b6caa5759bb1755adb3bb158e0
Takundei Gora <t.gora@jacobs-university.de>
2020-03-21 23:50:10 +0100

    SkyLight using Samples from CSampler3f

--- modules/core/LightSky.h
+++ modules/core/LightSky.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ILight.h"
-#include "Sampler.h"
+#include "Sampler3f.h"
 
 namespace rt {
 	/**
@@ -16,59 +16,32 @@ namespace rt {
 		 * @param intensity The emission color and strength of the light source
 		 * @param pSampler Pointer to the sampler to be used with the area light
 		 */
-		DllExport CLightSky(Vec3f intensity, float radius = std::numeric_limits<float>::infinity(), std::shared_ptr<CSampler> pSampler = std::make_shared<CSamplerStratified>(4, true, true), bool castShadow = true)
-			: ILight(castShadow)
-			, m_pSampler(pSampler)
+		DllExport CLightSky(Vec3f intensity, float radius = std::numeric_limits<float>::infinity(), std::shared_ptr<CSampler3f> dSampler = std::make_shared<CSampler3fTangent>())
+			: ILight(true)
 			, m_intensity(intensity)
 			, m_radius(radius)
+			,m_dSampler(dSampler)
 		{}
-
-		//namespace {
-		// TODO: Put in a utilities and merge with the code in CShaderChrome
-		std::pair<Vec3f, Vec3f> getRandomTangents(Vec3f normal)
-		{
-			std::pair<Vec3f, Vec3f> res;
-			float s1 = random::U<float>();
-			float s2 = random::U<float>();
-
-			if (fabs(normal.val[0]) > 0.1f)
-				res.first = Vec3f(-(s1 * normal.val[1] + s2 * normal.val[2]) / normal.val[0], s1, s2);
-			else if (fabs(normal.val[1]) > 0.1f)
-				res.first = Vec3f(s1, -(s1 * normal.val[0] + s2 * normal.val[2] / normal.val[1]), s2);
-			else
-				res.first = Vec3f(s1, s2, -(s1 * normal.val[0] + s2 * normal.val[1]) / normal.val[2]);
-
-			res.second = normal.cross(res.first);
-			res.first = normalize(res.first);
-			res.second = normalize(res.second);
-			return res;
-		}
-		//}
 
 		DllExport virtual std::optional<Vec3f> illuminate(Ray& ray) override
 		{
-			// TODO: Maybe optimize it using the cosine sampler
-			Vec2f sample = m_pSampler->getNextSample();
 			ray.t = 0;
 			Vec3f normal = ray.hit->getNormal(ray);
-			std::pair<Vec3f, Vec3f> tangents = getRandomTangents(normal);
-
-			Vec3f disp = (2 * sample.val[0] - 1) * tangents.first + (2 * sample.val[1] - 1) * tangents.second;
-
+			ray.dir = m_dSampler->getNextSample(normal);
 	
 			// ray towards point light position
 			ray.hit = nullptr;
-			ray.dir = normalize(normal + disp);
 			ray.t = m_radius;
 			
 			return m_intensity;
 		}
+		DllExport virtual size_t getNumberOfSamples(void) const override { return m_dSampler->getNumSamples(); }
 
-		DllExport virtual size_t getNumberOfSamples(void) const override { return m_pSampler->getNumSamples(); }
+
 
 	private:
-		std::shared_ptr<CSampler> m_pSampler;
 		Vec3f m_intensity;
 		float m_radius;
+		std::shared_ptr<CSampler3f> m_dSampler;
 	};
 }
