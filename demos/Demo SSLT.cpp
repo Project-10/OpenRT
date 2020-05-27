92e48fa3baa3f0be5087ae3b4d58949b69888514
Ezana Geressu <ezanageressu@gmail.com>
2020-05-27 17:44:25 +0200

    SSLT demo corrections

--- demos/Demo SSLT.cpp
+++ demos/Demo SSLT.cpp
@@ -5,31 +5,34 @@ using namespace rt;
 
 int main() {
 	CScene scene(Vec3f::all(0.0f));
-	auto pShaderFloor = std::make_shared<CShaderFlat>(Vec3f::all(1));
 
-	float s = 50;
+	auto pShaderFloor = std::make_shared<CShaderPhong>(scene, RGB(1, 1, 5), 0.1f, 0.9f, 0.9f, 40.0f);
+	auto pShaderWall = std::make_shared<CShaderPhong>(scene, RGB(1, 1, 5), 0.1f, 0.9f, 0.9f, 40.0f);
+	auto pShaderBall = std::make_shared<CShaderFlat>(RGB(0.5f,0.5f,0.5f));// , 0.1f, 0.5f, 0.2f, 40.0f);
+
+	float s = 4;
 	//Ball
-	scene.add(std::make_shared<CPrimSphere>(pShaderBall, Vec3f(0, 1, 0), 0.5f));
+	scene.add(std::make_shared<CPrimSphere>(pShaderBall, Vec3f(0, 0, 1), 1));
 	//Floor
-	scene.add(CSolidQuad(pShaderFloor, Vec3f(-s, 0, -s), Vec3f(-s, 0, s), Vec3f(s, 0, s), Vec3f(s, 0, -s)));
+	scene.add(CSolidQuad(pShaderFloor, Vec3f(-s, s, 0), Vec3f(s, s, 0), Vec3f(s, -s, 0), Vec3f(-s, -s, 0)));
+	//Walls
+	scene.add(CSolidQuad(pShaderWall, Vec3f(-s, s, 0), Vec3f(s, s, 0), Vec3f(s, s, s), Vec3f(-s, s, s)));
+	scene.add(CSolidQuad(pShaderWall, Vec3f(-s, s, 0), Vec3f(-s, -s, 0), Vec3f(-s, -s, s), Vec3f(-s, s, s)));
+	scene.add(CSolidQuad(pShaderWall, Vec3f(s, s, 0), Vec3f(s, -s, 0), Vec3f(s, -s, s), Vec3f(s, s, s)));
 
+	auto pCamera = std::make_shared<CCameraPerspectiveTarget>(Vec3f(0, -8, 5), Vec3f(0, 0, 1), Vec3f(0, 0, 1), 50.0f, Size(640, 480));
+	auto pLightPoint = std::make_shared<CLightPoint>(RGB(25, 25, 25), Vec3f(0, -20, 5));
 
-	auto pCamera = std::make_shared<CCameraPerspectiveTarget>(Vec3f(4, 4, 4), Vec3f(0, 0.5f, 0), Vec3f(0, 1, 0), 45.0f, Size(640, 480));
-	auto pLightPoint = std::make_shared<CLightPoint>(RGB(10, 10, 10), Vec3f(4, 4, 0));
-	auto pLightSpot = std::make_shared<CLightSpotDirected>(RGB(10, 10, 10), Vec3f(4, 4, 0), Vec3f(0, 0, 0), 20.0f, 10.0f);
 	scene.add(pCamera);
-	if (false) scene.add(pLightPoint);
-	else scene.add(pLightSpot);
-
-
-
+	scene.add(pLightPoint);
+	
 #ifdef ENABLE_BSP
 	scene.buildAccelStructure();
 #endif
 
 	Mat img = scene.render(std::make_shared<CSamplerStratified>(2));
-	imshow("image", img);
+	imshow("Subsurface Scattering", img);
 	waitKey();
-	imwrite("out1.jpg", img);
+	imwrite("SSLTout1.jpg", img);
 	return 0;
 }
