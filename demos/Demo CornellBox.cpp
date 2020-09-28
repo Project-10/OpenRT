81b1e13a6e20754af5c0ec3c39d260c58714f3cc
Sergey Kosov <sergey.kosov@project-10.de>
2020-09-28 18:32:27 +0200

    Update Demo CornellBox.cpp

--- demos/Demo CornellBox.cpp
+++ demos/Demo CornellBox.cpp
@@ -5,10 +5,15 @@ using namespace rt;
 
 int main()
 {
-	CScene scene;
+	const Vec3f	bgColor = RGB(0, 0, 0);
+	const Size	resolution = Size(1024, 1024);
+	const float intensity = 5000;
+
+	// Scene
+	CScene scene(bgColor);
 	
 	// Camera
-	auto pCamera = std::make_shared<CCameraPerspective>(Size(512, 512), Vec3f(278, 273, -800), Vec3f(0, 0, 1), Vec3f(0, 1, 0), 39.3f);
+	auto pCamera = std::make_shared<CCameraPerspective>(resolution, Vec3f(278, 273, -800), Vec3f(0, 0, 1), Vec3f(0, 1, 0), 39.3f);
 	
 	// Shaders
 	auto pShaderLight	= std::make_shared<CShaderFlat>(RGB(1, 1, 1));
@@ -18,10 +23,9 @@ int main()
 	auto pShader		= std::make_shared<CShader>(scene, RGB(1, 1, 1), 0, 0.5f, 0, 0, 0.5f, 0, 0, std::make_shared<CSamplerStratified>(4, true, true));
 	
 	// Lights
-	auto pLight			= std::make_shared<CLightArea>(10000 * RGB(1.0f, 0.839f, 0.494f), Vec3f(343, 548.78f, 227), Vec3f(343, 548.78f, 332), Vec3f(213, 548.78f, 332), Vec3f(213, 548.78f, 227), std::make_shared<CSamplerStratified>(6, true, true));
-	float d = 100;
-	auto pLightGreen	= std::make_shared<CLightArea>(RGB(0, 50, 0), Vec3f(0.1f, d, 559.2f - d), Vec3f(0.1f, d, d), Vec3f(0.1f, 548.8f - d, d), Vec3f(0.1f, 548.8f - d, 559.2f - d),  std::make_shared<CSamplerStratified>(6, true, true));
-	auto pLightRed		= std::make_shared<CLightArea>(RGB(50, 0, 0), Vec3f(552.7f, d, d), Vec3f(549.5f, d, 559.2f - d), Vec3f(555.9f, 548.8f - d, 559.2f - d), Vec3f(555.9f, 548.8f - d, d), std::make_shared<CSamplerStratified>(6, true, true));
+	auto pLight			= std::make_shared<CLightArea>(intensity * RGB(1.0f, 0.839f, 0.494f), Vec3f(343, 548.78f, 227), Vec3f(343, 548.78f, 332), Vec3f(213, 548.78f, 332), Vec3f(213, 548.78f, 227), std::make_shared<CSamplerStratified>(6, true, true));
+	auto pLightGreen	= std::make_shared<CLightOmni>(20 * intensity * RGB(0, 1, 0), Vec3f(-1000, 400, 280), false);
+	auto pLightRed		= std::make_shared<CLightOmni>(20 * intensity * RGB(1, 0, 0), Vec3f(1553.7f, 400, 280), false);
 	
 	// Blocks
 	CSolidBox shortBlock(pShaderWhite, Vec3f(185.5f, 82.5f, 169), 165, 165, 168);
@@ -46,15 +50,13 @@ int main()
 	scene.add(shortBlock);
 	scene.add(tallBlock);
 	
-#ifdef ENABLE_BSP
-	scene.buildAccelStructure();
-#endif
+	scene.buildAccelStructure(0, 3);
 	
 	Timer::start("Rendering... ");
-	Mat img = scene.render(std::make_shared<CSamplerStratified>(2, true, true));
+	Mat img = scene.render(std::make_shared<CSamplerStratified>(4, true, true));
 	Timer::stop();
 	imshow("image", img);
-	imwrite("cornell box.jpg", img);
+	imwrite("D:\\renders\\res.jpg", img);
 	waitKey();
 	return 0;
 }
