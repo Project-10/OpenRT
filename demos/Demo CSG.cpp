d694ce496acddfccbf3807f6dc1d12f26b1cf8ad
Otmane Sabir <o.sabir@jacobs-university.de>
2020-11-10 21:03:15 +0100

    modification to simple CSG

--- demos/Demo CSG.cpp
+++ demos/Demo CSG.cpp
@@ -18,7 +18,7 @@ int main()
     auto pShaderBlue	= std::make_shared<CShaderEyelight>(RGB(0, 0, 1));
 
     //Floor
-    scene.add(std::make_shared<CCompositeGeometry>(pShaderRed, CSolidCylinder(pShaderRed, Vec3f(4, 0, 0), 1.2, 5), CSolidCylinder(pShaderBlue, Vec3f(5, 0, 0), 1.5, 5), rt::Union));
+    scene.add(std::make_shared<CCompositeGeometry>(pShaderRed, CSolidCylinder(pShaderBlue, Vec3f(5, 0, 0),1.5, 5), CSolidCylinder(pShaderRed, Vec3f(4, 0, 0), 1.2, 5), rt::Intersection));
     //scene.add(std::make_shared<CCompositeGeometry>(pShaderBlue, CSolidCylinder(pShaderRed, Vec3f(5, 0, 0), 2, 5), CSolidCylinder(pShaderBlue2, Vec3f(4, 0, 0), 2, 5), rt::Intersection));
     //scene.add(CSolidCylinder(pShaderBlue, Vec3f(4, 0, 0), 2, 5));
     //scene.add(CSolidCylinder(pShaderRed, Vec3f(5, 0, 0), 2, 5));
@@ -26,7 +26,7 @@ int main()
     // camera
     auto mainCam = std::make_shared<CCameraPerspective>(resolution, Vec3f(5, 8, 8), Vec3f(0, -0.5, -1), Vec3f(0, 1, 0), 60.0f);
 
-    //scene.buildAccelStructure();
+    scene.buildAccelStructure();
 
     // Light
     auto pLight			= std::make_shared<CLightArea>(intensity * RGB(1.0f, 0.839f, 0.494f), Vec3f(343, 548.78f, 227), Vec3f(343, 548.78f, 332), Vec3f(213, 548.78f, 332), Vec3f(213, 548.78f, 227), std::make_shared<CSamplerStratified>(6, true, true));
