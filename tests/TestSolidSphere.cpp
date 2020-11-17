00837e1767afcb24da244fbbae15cc1c8b2ee5bf
Otmane Sabir <o.sabir@jacobs-university.de>
2020-11-17 21:23:37 +0100

    fix appVeyor issues

--- tests/TestSolidSphere.cpp
+++ tests/TestSolidSphere.cpp
@@ -1,9 +1,9 @@
-﻿#include "TestCamera.h"
-#include "core/Ray.h"
+﻿#include "core/Ray.h"
+#include "TestSolidSphere.h"
 
 using namespace rt;
 
-TEST(CTestSolidSphere, solid_sphere) {
+TEST_F(CTestSolidSphere, solid_sphere) {
     auto shader = std::make_shared<CShaderFlat>(RGB(1, 1, 1));
     auto sphere = CSolidSphere(shader, Vec3f::all(0), 2, 10);
     // we multiply by 2 because each quad contains 2 triangles.
@@ -11,7 +11,7 @@ TEST(CTestSolidSphere, solid_sphere) {
 }
 
 // Test that checks that the solid sphere is within the radius value.
-TEST(CTestSolidSphere, solid_sphere_box) {
+TEST_F(CTestSolidSphere, solid_sphere_box) {
     auto radius = 3.0f;
     auto shader = std::make_shared<CShaderFlat>(RGB(1, 1, 1));
     auto sphere = CSolidSphere(shader, Vec3f::all(0), radius, 10);
---
+++ tests/TestSolidSphere.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "gtest/gtest.h"
+#include "types.h"
+#include "openrt.h"
+
+class CTestSolidSphere : public ::testing::Test {
+public:
+    CTestSolidSphere(void) = default;
+	~CTestSolidSphere(void) = default;
+};
