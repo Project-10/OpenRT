02b9b0a9d6354cc7e9e59e638331c695cafdac15
Otmane Sabir <o.sabir@jacobs-university.de>
2020-11-17 21:48:36 +0100

    attempt #3

--- tests/TestSolidSphere.cpp
+++ tests/TestSolidSphere.cpp
@@ -1,20 +1,14 @@
-﻿#include "core/Ray.h"
-#include "TestSolidSphere.h"
+﻿#include "TestSolidSphere.h"
 
 using namespace rt;
 
 TEST_F(CTestSolidSphere, solid_sphere) {
     auto shader = std::make_shared<CShaderFlat>(RGB(1, 1, 1));
-    auto sphere = CSolidSphere(shader, Vec3f::all(0), 2, 10);
+    auto radius = 3.0f;
+    auto sphere = CSolidSphere(shader, Vec3f::all(0), radius, 10);
     // we multiply by 2 because each quad contains 2 triangles.
     EXPECT_EQ(2*(11)*10, sphere.getPrims().size());
-}
 
-// Test that checks that the solid sphere is within the radius value.
-TEST_F(CTestSolidSphere, solid_sphere_box) {
-    auto radius = 3.0f;
-    auto shader = std::make_shared<CShaderFlat>(RGB(1, 1, 1));
-    auto sphere = CSolidSphere(shader, Vec3f::all(0), radius, 10);
     // we multiply by 2 because each quad contains 2 triangles.
     CBoundingBox box;
     for (const auto& prim : sphere.getPrims())
