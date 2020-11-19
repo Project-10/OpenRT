cc53e86c99c0ed12f07575b8ab9919d11dbf2fdb
Otmane Sabir <o.sabir@jacobs-university.de>
2020-11-19 16:57:00 +0100

    added DLLExport to AABB

--- modules/core/BoundingBox.h
+++ modules/core/BoundingBox.h
@@ -19,12 +19,12 @@ namespace rt {
 		 * @param minPoint The minimal point defying the size of the bounding box
 		 * @param maxPoint The maximal point defying the size of the bounding box
 		 */
-		CBoundingBox(const Vec3f& minPoint = Vec3f::all(Infty), const Vec3f& maxPoint = Vec3f::all(-Infty))
+        DllExport explicit CBoundingBox(const Vec3f& minPoint = Vec3f::all(Infty), const Vec3f& maxPoint = Vec3f::all(-Infty))
 			: m_minPoint(minPoint)
 			, m_maxPoint(maxPoint)
 		{}
-		~CBoundingBox(void) = default;
-		friend std::ostream& operator<<(std::ostream& os, const CBoundingBox& box) {
+        DllExport ~CBoundingBox(void) = default;
+        DllExport friend std::ostream& operator<<(std::ostream& os, const CBoundingBox& box) {
 			os << box.m_minPoint << " " << box.m_maxPoint;
 			return os;
 		}
@@ -33,12 +33,12 @@ namespace rt {
 		 * @brief Extends the bounding box to contain point \b p
 		 * @param p A point
 		 */
-		void extend(const Vec3f& p);
+        DllExport void extend(const Vec3f& p);
 		/**
 		 * @brief Extends the bounding box to contain bounding box \b box
 		 * @param box The second bounding box
 		 */
-		void extend(const CBoundingBox& box);
+        DllExport void extend(const CBoundingBox& box);
 		/**
          * @brief Splits the bounding box into two parts
          * @details This function splits the bounding box with a plane orthogonal to the axis \b dim. Two resulting bounding boxes are initialized and returned as the result.
@@ -46,12 +46,12 @@ namespace rt {
          * @param val The value for the defined dimension where the box is splitted. It must be in range defined my minimal and maximal points of the bounding box
          * @returns A pair of bounding boxes, as a matter of fact "left" and "right" bounding boxes
          */
-        std::pair<CBoundingBox, CBoundingBox> split(int dim, float val) const;
+        DllExport std::pair<CBoundingBox, CBoundingBox> split(int dim, float val) const;
         /**
 		 * @brief Checks if the current bounding box overlaps with the argument bounding box \b box
 		 * @param box The secind bounding box to be checked with
 		 */
-		bool overlaps(const CBoundingBox& box) const;
+        DllExport bool overlaps(const CBoundingBox& box) const;
         /**
          * @brief Clips the ray with the bounding box
          * @details If ray \b ray does not intersect the bounding box, resulting t1 will be smaller than t0
@@ -60,17 +60,17 @@ namespace rt {
          * @param[in,out] t0 The distance from ray origin at which the ray enters the bounding box
          * @param[in,out] t1 The distance from ray origin at which the ray leaves the bounding box
          */
-		void clip(const Ray& ray, double& t0, double& t1) const;
+        DllExport void clip(const Ray& ray, double& t0, double& t1) const;
         /**
          * @brief Returns the minimal point defying the size of the bounding box
          * @returns The minimal point defying the size of the bounding box
          */
-        Vec3f getMinPoint(void) const { return m_minPoint; }
+        DllExport Vec3f getMinPoint(void) const { return m_minPoint; }
         /**
          * @brief Returns the maximal point defying the size of the bounding box
          * @returns The maximal point defying the size of the bounding box
          */
-        Vec3f getMaxPoint(void) const { return m_maxPoint; }
+        DllExport Vec3f getMaxPoint(void) const { return m_maxPoint; }
 		
         
 	private:
