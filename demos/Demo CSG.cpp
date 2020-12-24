2a6470707acfdab10cacc86b4102006f6ca5b0b2
Otmane Sabir <o.sabir@jacobs-university.de>
2020-12-24 20:41:30 +0100

    code reformat

--- demos/Demo CSG.cpp
+++ demos/Demo CSG.cpp
@@ -5,29 +5,30 @@
 
 using namespace rt;
 
-int main()
-{
-    const Vec3f	bgColor = RGB(0.3, 0.3,0.3);
-    const Size	resolution = Size(1920, 1200);
+int main() {
+    const Vec3f bgColor = RGB(0.3, 0.3, 0.3);
+    const Size resolution = Size(1920, 1200);
     const float intensity = 5e4;
 
     // Scene
     CScene scene(bgColor);
 
-    auto pTextureEarth  = std::make_shared<CTexture>("/Users/otmanesabir/Desktop/S5/Project&Thesis/OpenRT/data/1_earth_8k.jpg");
-    auto pShaderEarth 	= std::make_shared<CShaderPhong>(scene, pTextureEarth, 0.2f, 0.7f, 0.0f, 40.0f);
+    auto pTextureEarth = std::make_shared<CTexture>(dataPath + "1_earth_8k.jpg");
+    auto pShaderEarth = std::make_shared<CShaderPhong>(scene, pTextureEarth, 0.2f, 0.7f, 0.0f, 40.0f);
 
     // Shaders
-   auto pDarkBlue = std::make_shared<CShaderPhong>(scene, RGB(47/255.0, 60/255.0, 126/255.0), 0.1f, 0.9f, 0, 40.0f);
-   auto pSunsetOrange = std::make_shared<CShaderPhong>(scene, RGB(251/255.0, 234/255.0, 235/255.0), 0.1f, 0.9f, 0, 40.0f);
+    auto pDarkBlue = std::make_shared<CShaderPhong>(scene, RGB(47 / 255.0, 60 / 255.0, 126 / 255.0), 0.1f, 0.9f, 0,
+                                                    40.0f);
+    auto pSunsetOrange = std::make_shared<CShaderPhong>(scene, RGB(251 / 255.0, 234 / 255.0, 235 / 255.0), 0.1f, 0.9f,
+                                                        0, 40.0f);
     auto pShaderFloor = std::make_shared<CShaderPhong>(scene, RGB(1, 1, 1), 0.1f, 0.9f, 0.0f, 40.0f);
     // auto pShaderFloor = std::make_shared<CShaderEyelight>(RGB(1, 1, 1));
     // auto pShaderRed = std::make_shared<CShaderEyelight>(RGB(1, 0, 0));
     // auto pShaderBlue = std::make_shared<CShaderEyelight>(RGB(0, 0, 1));
 
     // Geometries
-    auto solidSphere1 = CSolidSphere(pDarkBlue,  Vec3f(1, 0.1f, -13), 1.5, 24, true);
-    auto solidSphere2 = CSolidSphere(pSunsetOrange,  Vec3f(0, 0.1f, -13), 1.5, 24, true);
+    auto solidSphere1 = CSolidSphere(pDarkBlue, Vec3f(1, 0.1f, -13), 1.5, 24, true);
+    auto solidSphere2 = CSolidSphere(pSunsetOrange, Vec3f(0, 0.1f, -13), 1.5, 24, true);
     scene.add(std::make_shared<CCompositeGeometry>(pDarkBlue, solidSphere1, solidSphere2, rt::Union));
 
     // Try it with Cylinders!
@@ -41,11 +42,16 @@ int main()
 
     // cameras
     const float r = 10;
-    auto front = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 7, 8), Vec3f(0, -0.4, -1), Vec3f(0, 1, 0), 45.0f);
-    auto back = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 20, -30), Vec3f(0, -1, 1), Vec3f(0, 1, 0), 45.0f);
-    auto top = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 22,-5), Vec3f(0, -1, -0.5), Vec3f(0, 0, -1), 45.0f);
-    auto cam45 = std::make_shared<CCameraPerspective>(resolution, Vec3f(35, 20, -30), Vec3f(-2, -1.1, 1), Vec3f(0, 1, 0), 45.0f);
-    auto frontTilted = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 22, 8), Vec3f(0, -1, -1), Vec3f(0, 1, 0), 45.0f);
+    auto front = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 7, 8), Vec3f(0, -0.4, -1), Vec3f(0, 1, 0),
+                                                      45.0f);
+    auto back = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 20, -30), Vec3f(0, -1, 1), Vec3f(0, 1, 0),
+                                                     45.0f);
+    auto top = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 22, -5), Vec3f(0, -1, -0.5), Vec3f(0, 0, -1),
+                                                    45.0f);
+    auto cam45 = std::make_shared<CCameraPerspective>(resolution, Vec3f(35, 20, -30), Vec3f(-2, -1.1, 1),
+                                                      Vec3f(0, 1, 0), 45.0f);
+    auto frontTilted = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 22, 8), Vec3f(0, -1, -1),
+                                                            Vec3f(0, 1, 0), 45.0f);
     scene.add(cam45);
 
     scene.buildAccelStructure(20, 2);
@@ -62,13 +68,13 @@ int main()
 
     std::string counterPath = "../../../num.txt";
 
-    int renderCount=0;
-    std::ifstream i(counterPath,std::ios::binary);
-    i.read((char*)&renderCount,sizeof(renderCount));
+    int renderCount = 0;
+    std::ifstream i(counterPath, std::ios::binary);
+    i.read((char *) &renderCount, sizeof(renderCount));
     imwrite("../../../render_" + std::to_string(renderCount) + ".png", img);
     renderCount++;
-    std::ofstream o(counterPath,std::ios::binary);
-    o.write((char*)&renderCount,sizeof(renderCount));
+    std::ofstream o(counterPath, std::ios::binary);
+    o.write((char *) &renderCount, sizeof(renderCount));
     o.close();
     imshow("image", img);
     waitKey();
--- modules/core/CompositeGeometry.h
+++ modules/core/CompositeGeometry.h
@@ -4,28 +4,34 @@
 #include "Solid.h"
 
 namespace rt {
-    enum OperationType {Intersection, Subtraction, Union};
+    enum OperationType {
+        Intersection, Subtraction, Union
+    };
+
     class CCompositeGeometry : public IPrim {
     public:
-        DllExport explicit CCompositeGeometry(ptr_shader_t pShader, const CSolid& s1, const CSolid& s2, OperationType operationType);
+        DllExport explicit CCompositeGeometry(ptr_shader_t pShader, const CSolid &s1, const CSolid &s2,
+                                              OperationType operationType);
 
-        DllExport explicit CCompositeGeometry(const rt::ptr_shader_t& pShader,
+        DllExport explicit CCompositeGeometry(const rt::ptr_shader_t &pShader,
                                               const rt::CSolid &s1, const rt::ptr_prim_t &p2,
                                               rt::OperationType operationType);
 
-        DllExport explicit CCompositeGeometry(const ptr_shader_t &pShader, const ptr_prim_t &p1, const ptr_prim_t &p2, OperationType operationType);
+        DllExport explicit CCompositeGeometry(const ptr_shader_t &pShader, const ptr_prim_t &p1, const ptr_prim_t &p2,
+                                              OperationType operationType);
 
-        DllExport bool 			intersect(Ray& ray) const override;
+        DllExport bool intersect(Ray &ray) const override;
 
-        DllExport bool 			if_intersect(const Ray& ray) const override;
+        DllExport bool if_intersect(const Ray &ray) const override;
 
-        DllExport void 			transform(const Mat& T) override;
+        DllExport void transform(const Mat &T) override;
 
-        DllExport Vec3f 		getNormal(const Ray& ray) const override;
+        DllExport Vec3f getNormal(const Ray &ray) const override;
 
-        DllExport Vec2f			getTextureCoords(const Ray& ray) const override;
+        DllExport Vec2f getTextureCoords(const Ray &ray) const override;
 
-        DllExport CBoundingBox	getBoundingBox() const override;
+        DllExport CBoundingBox getBoundingBox() const override;
+
     private:
         CSolid m_s1;
         CSolid m_s2;
