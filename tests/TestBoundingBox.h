8b3c425c81b587c48dd6392b78e7f623b87b2067
Otmane Sabir <o.sabir@jacobs-university.de>
2021-02-07 13:51:29 +0100

    added test cases

---
+++ tests/TestBoundingBox.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "gtest/gtest.h"
+#include "types.h"
+#include "openrt.h"
+
+class CTestBoundingBox : public ::testing::Test {
+public:
+    CTestBoundingBox(void) = default;
+    ~CTestBoundingBox(void) = default;
+};
