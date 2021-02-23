b0139ebc6fe18396db2f8b4af9f5bbf8927127c8
Otmane <sabir@Otmanes-MacBook-Pro.local>
2021-02-23 19:16:37 +0100

    add new class in include

--- include/openrt.h
+++ include/openrt.h
@@ -24,6 +24,7 @@
 #include "core/SolidCone.h"
 #include "core/SolidCylinder.h"
 #include "core/SolidSphere.h"
+#include "core/SolidTorus.h"
 
 #include "core/Shader.h"
 #include "core/ShaderFlat.h"
---
+++ tests/TestSolidTorus.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "gtest/gtest.h"
+#include "types.h"
+#include "openrt.h"
+
+class CTestSolidTorus : public ::testing::Test {
+public:
+
+    CTestSolidTorus(void) = default;
+
+    ~CTestSolidTorus(void) = default;
+};
