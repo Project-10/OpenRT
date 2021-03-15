03edc6b598beb64a5058a7e576fe502d3e95b906
Otmane Sabir <o.sabir@jacobs-university.de>
2021-03-15 11:52:24 +0100

    fixed torus unit test

--- tests/TestSolidTorus.cpp
+++ tests/TestSolidTorus.cpp
@@ -11,8 +11,8 @@ TEST_F(CTestSolidTorus, solid_torus) {
     auto torus = CSolidTorus(shader, Vec3f::all(0), innerRadius, outerRadius, sides);
 
     // we multiply by 2 because each quad contains 2 triangles.
-    // On n sides input number of tris is total = (n^2) * 2.
-    EXPECT_EQ((sides * sides) * 2, torus.getPrims().size());
+    // On n sides input number of tris is total = (n^2) * 4.
+    EXPECT_EQ((sides * sides) * 4, torus.getPrims().size());
 
     // Check assertions
     EXPECT_DEATH(CSolidTorus(shader, Vec3f::all(0), 2, 1, 2), "");
