c4375d17d09256c2715ab04378d38238230bb73c
Otmane Sabir <o.sabir@jacobs-university.de>
2021-03-16 23:22:57 +0100

    task-08 fix demo file

--- demos/Demo CSG.cpp
+++ demos/Demo CSG.cpp
@@ -4,65 +4,57 @@
 
 using namespace rt;
 
-void timeTests(const String& filePath) {
-    std::ofstream myFile;
-    myFile.open (filePath);
-    const Vec3f bgColor = RGB(0.3, 0.3, 0.3);
-    const Size resolution = Size(1920, 1200);
-    const float intensity = 5e4;
+ptr_prim_t createDice(ptr_shader_t pDiceShader) {
+    // First Side
+    auto solidSphere_S1_1 = CSolidSphere(pDiceShader, Vec3f(-15, 0, -13), 2, 100, true);
+    auto solidSphere_S1_2 = CSolidSphere(pDiceShader, Vec3f(-15, 7.5, -13), 2, 100, true);
+    auto solidSphere_S1_3 = CSolidSphere(pDiceShader, Vec3f(-15, -7.5, -13), 2, 100, true);
+    auto solidSphere_S1_4 = CSolidSphere(pDiceShader, Vec3f(-15, -7.5, -5.5), 2, 100, true);
+    auto solidSphere_S1_5 = CSolidSphere(pDiceShader, Vec3f(-15, -7.5, -20.5), 2, 100, true);
+    auto solidSphere_S1_6 = CSolidSphere(pDiceShader, Vec3f(-15, 7.5, -5.5), 2, 100, true);
+    auto solidSphere_S1_7 = CSolidSphere(pDiceShader, Vec3f(-15, 7.5, -20.5), 2, 100, true);
+    auto solidSphere_S1_8 = CSolidSphere(pDiceShader, Vec3f(-15, 0, -5.5), 2, 100, true);
+    auto solidSphere_S1_9 = CSolidSphere(pDiceShader, Vec3f(-15, 0, -20.5), 2, 100, true);
 
-    // Shaders
-    auto pShaderRed = std::make_shared<CShaderEyelight>(RGB(1, 0, 0));
-    auto pShaderBlue = std::make_shared<CShaderEyelight>(RGB(0, 0, 1));
+    // Second Side
+    auto solidSphere_S2_1 = CSolidSphere(pDiceShader, Vec3f(0, 0, -28), 2, 100, true);
+    auto solidSphere_S2_2 = CSolidSphere(pDiceShader, Vec3f(-7.5, 0, -28), 2, 100, true);
+    auto solidSphere_S2_3 = CSolidSphere(pDiceShader, Vec3f(7.5, 0, -28), 2, 100, true);
+    auto solidSphere_S2_4 = CSolidSphere(pDiceShader, Vec3f(0, 7.5, -28), 2, 100, true);
+    auto solidSphere_S2_5 = CSolidSphere(pDiceShader, Vec3f(-7.5, 7.5, -28), 2, 100, true);
+    auto solidSphere_S2_6 = CSolidSphere(pDiceShader, Vec3f(7.5, 7.5, -28), 2, 100, true);
+    auto solidSphere_S2_7 = CSolidSphere(pDiceShader, Vec3f(0, -7.5, -28), 2, 100, true);
+    auto solidSphere_S2_8 = CSolidSphere(pDiceShader, Vec3f(-7.5, -7.5, -28), 2, 100, true);
+    auto solidSphere_S2_9 = CSolidSphere(pDiceShader, Vec3f(7.5, -7.5, -28), 2, 100, true);
 
-    // Light
-    auto pLight = std::make_shared<CLightOmni>(intensity * RGB(1.0f, 0.839f, 0.494f), Vec3f(100, 150.0f, 100), false);
+    // Box
+    auto solidBox = CSolidBox(pDiceShader, Vec3f(Vec3f(0, 0, -13)), 30, 30, 30);
 
-    for (int i = 6; i < 206; i++) {
-        // Scene
-        CScene scene(bgColor);
-        // Geometries
-        auto 		solidSphere1 = CSolidSphere(pShaderRed, Vec3f(1, 0.1f, -13), 1.5f, i, false);
-        auto 		solidSphere2 = CSolidSphere(pShaderBlue, Vec3f(0, 0.1f, -13), 1.5f, i, false);
-        auto 	pComposize	 = std::make_shared<CCompositeGeometry>(solidSphere2, solidSphere1, BoolOp::Union);
-
-        // Camera
-        auto targetCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(5, 10, -10), pComposize->getBoundingBox().getCenter(), Vec3f(0, 1, 0), 45.0f);
-        scene.add(targetCamera);
-        scene.add(pLight);
-        scene.add(pComposize);
-        auto nPrimitives = solidSphere1.getPrims().size() +  solidSphere2.getPrims().size();
-        pComposize->buildAccelStructure();
-        scene.buildAccelStructure(20, 2);
-
-
-
-        auto t1 = std::chrono::high_resolution_clock::now();
-        Mat image = scene.render();
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
-
-        // log count to file
-        myFile << nPrimitives << "," << duration << std::endl;
-        std::cout << nPrimitives << "," << duration << std::endl;
-        std::cout << "Progress: " << i/206.0 * 100.0 << "%" << std::endl;
-
-        std::string counterPath = "../../num.txt";
-        int renderCount = 0;
-        std::ifstream countFile(counterPath, std::ios::binary);
-        countFile.read((char *) &renderCount, sizeof(renderCount));
-        countFile.close();
-        imwrite("../../timeTestsRenders/optim_bsp_union_" + std::to_string(renderCount) + ".png", image);
-        renderCount++;
-        std::ofstream o(counterPath, std::ios::binary);
-        o.write((char *) &renderCount, sizeof(renderCount));
-        o.close();
-    }
-    myFile.close();
+    // All together
+    ptr_prim_t pComposize_1	 = std::make_shared<CCompositeGeometry>(solidBox, solidSphere_S2_1, BoolOp::Difference);
+    ptr_prim_t pComposize_2	 = std::make_shared<CCompositeGeometry>(pComposize_1, solidSphere_S2_2, BoolOp::Difference);
+    ptr_prim_t pComposize_3	 = std::make_shared<CCompositeGeometry>(pComposize_2, solidSphere_S2_3, BoolOp::Difference);
+    ptr_prim_t pComposize_4	 = std::make_shared<CCompositeGeometry>(pComposize_3, solidSphere_S2_4, BoolOp::Difference);
+    ptr_prim_t pComposize_5	 = std::make_shared<CCompositeGeometry>(pComposize_4, solidSphere_S2_5, BoolOp::Difference);
+    ptr_prim_t pComposize_6	 = std::make_shared<CCompositeGeometry>(pComposize_5, solidSphere_S2_6, BoolOp::Difference);
+    ptr_prim_t pComposize_7 = std::make_shared<CCompositeGeometry>(pComposize_6, solidSphere_S2_7, BoolOp::Difference);
+    ptr_prim_t pComposize_8	 = std::make_shared<CCompositeGeometry>(pComposize_7, solidSphere_S2_8, BoolOp::Difference);
+    ptr_prim_t pComposize_9	 = std::make_shared<CCompositeGeometry>(pComposize_8, solidSphere_S2_9, BoolOp::Difference);
+    ptr_prim_t pComposize	 = std::make_shared<CCompositeGeometry>(pComposize_9, solidSphere_S1_1, BoolOp::Difference);
+    ptr_prim_t pComposize2	 = std::make_shared<CCompositeGeometry>(pComposize, solidSphere_S1_2, BoolOp::Difference);
+    ptr_prim_t pComposize3	 = std::make_shared<CCompositeGeometry>(pComposize2, solidSphere_S1_3, BoolOp::Difference);
+    ptr_prim_t pComposize4	 = std::make_shared<CCompositeGeometry>(pComposize3, solidSphere_S1_4, BoolOp::Difference);
+    ptr_prim_t pComposize5	 = std::make_shared<CCompositeGeometry>(pComposize4, solidSphere_S1_5, BoolOp::Difference);
+    ptr_prim_t pComposize6	 = std::make_shared<CCompositeGeometry>(pComposize5, solidSphere_S1_6, BoolOp::Difference);
+    ptr_prim_t pComposize7	 = std::make_shared<CCompositeGeometry>(pComposize6, solidSphere_S1_7, BoolOp::Difference);
+    ptr_prim_t pComposize8	 = std::make_shared<CCompositeGeometry>(pComposize7, solidSphere_S1_8, BoolOp::Difference);
+    ptr_prim_t pComposize9	 = std::make_shared<CCompositeGeometry>(pComposize8, solidSphere_S1_9, BoolOp::Difference);
+    return pComposize9;
 }
 
-void sampleTest() {
-    const Vec3f bgColor = RGB(1, 1, 1);
+
+int main() {
+    const Vec3f bgColor = RGB(0, 0, 0);
     const Size resolution = Size(1920, 1200);
     const float intensity = 5e4;
 
@@ -70,50 +62,35 @@ void sampleTest() {
     CScene scene(bgColor);
 
     // Shaders
-    auto pDarkBlue = std::make_shared<CShaderPhong>(scene, RGB(47 / 255.0, 60 / 255.0, 126 / 255.0), 0.1f, 0.9f, 0, 40.0f);
-    auto pSunsetOrange = std::make_shared<CShaderPhong>(scene, RGB(251 / 255.0, 234 / 255.0, 235 / 255.0), 0.1f, 0.9f, 0, 40.0f);
-    auto pShaderFloor = std::make_shared<CShaderPhong>(scene, RGB(1, 1, 1), 0.1f, 0.9f, 0.0f, 40.0f);
-
-    auto pShaderBlue = std::make_shared<CShaderEyelight>(RGB(240.0/ 255.0, 240.0/255.0, 230.0/255.0));
-    auto pShaderOrange = std::make_shared<CShaderEyelight>((217.0/ 255.0, 4.0/255.0, 41/255.0));
-
+    auto pDiceShader = std::make_shared<CShaderPhong>(scene, RGB(0.447, 0.345, 0.019), 0.4f, 0.9f, 0.3f, 40.0f);
+    auto pShaderDice = std::make_shared<CShaderPhong>(scene, RGB(0.153f, 0.682f, 0.376f), 0.2f, 0.8f, 0.5f, 40.0f);
 
     // Geometries
-    auto solidSphere1 = CSolidSphere(pShaderBlue, Vec3f(1, 0.1f, -13), 1.3f, 30, false);
-    auto solidBox = CSolidBox(pShaderOrange, Vec3f(Vec3f(1, 0.1f, -13)), 2, 2, 2);
-    auto solidSphere2 = CSolidSphere(pShaderOrange, Vec3f(0, 0.1f, -13), 1.5f, 200, true);
-    auto pComposize	 = std::make_shared<CCompositeGeometry>(solidSphere1, solidSphere2, BoolOp::Union
-    );
-    auto solidCylinder2 = CSolidCylinder(pDarkBlue, Vec3f(1, 0.1f, -13),1.5, 4.1f);
-    scene.add(pComposize);
+    auto pComposite = createDice(pShaderDice);
+    scene.add(pComposite);
+
+    // Floor
+    auto pGlossinessSampler = std::make_shared<CSamplerRandom>(2);
+    auto pShaderFloor = std::make_shared<CShaderChrome>(scene, pGlossinessSampler);
+    float s = 1000;
+    scene.add(CSolidQuad(pShaderFloor, Vec3f(-s, -25, -s), Vec3f(-s, -25, s), Vec3f(s, -25, s), Vec3f(s, -25, -s)));
 
     // cameras
-    const float r = 10;
-    auto front = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 7, 8), Vec3f(0, -0.4, -1), Vec3f(0, 1, 0), 45.0f);
-    auto back = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 20, -30), Vec3f(0, -1, 1), Vec3f(0, 1, 0), 45.0f);
-    auto top = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 22, -5), Vec3f(0, -1, -0.5), Vec3f(0, 0, -1), 45.0f);
-    auto cam45 = std::make_shared<CCameraPerspective>(resolution, Vec3f(35, 20, -30), Vec3f(-2, -1.1, 1), Vec3f(0, 1, 0), 45.0f);
-    auto frontTilted = std::make_shared<CCameraPerspective>(resolution, Vec3f(0, 22, 8), Vec3f(0, -1, -1), Vec3f(0, 1, 0), 45.0f);
-    auto targetCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-2, 3, -3), pComposize->getBoundingBox().getCenter(), Vec3f(0, 1, 0), 45.0f);
+    auto targetCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-125, 10, -100), pComposite->getBoundingBox().getCenter(), Vec3f(0, 1, 0), 45.0f);
     scene.add(targetCamera);
 
-    //scene.buildAccelStructure(20, 2);
-    //pComposize->buildAccelStructure();
+    scene.buildAccelStructure(20, 2);
 
     // Light
-    auto pLight = std::make_shared<CLightOmni>(intensity * RGB(1.0f, 0.839f, 0.494f), Vec3f(100, 150.0f, 100), false);
+    auto pLight = std::make_shared<CLightOmni>(intensity * RGB(1.0f, 0.839f, 0.494f), Vec3f(-120, 200.0f, -100), true);
 
     // Add everything to scene
     scene.add(pLight);
     Timer::start("Rendering... ");
-    Mat img = scene.render();
+    Mat img = scene.render(std::make_shared<CSamplerStratified>(2, true, true));
     Timer::stop();
 
     imshow("image", img);
     waitKey();
-}
-
-int main() {
-    sampleTest();
     return 0;
 }
