255cacac86b81c4c1d91363fd3cf01953c9cbe94
Otmane Sabir <o.sabir@jacobs-university.de>
2021-05-04 00:03:59 +0200

    add Demo CSG logic

--- demos/Demo CSG.cpp
+++ demos/Demo CSG.cpp
@@ -14,30 +14,32 @@ int main() {
     CScene scene(bgColor);
 
     // Shaders
-    auto pDarkBlue = std::make_shared<CShaderPhong>(scene, RGB(47 / 255.0, 60 / 255.0, 126 / 255.0), 0.1f, 0.9f, 0, 40.0f);
-    auto pSunsetOrange = std::make_shared<CShaderPhong>(scene, RGB(251 / 255.0, 234 / 255.0, 235 / 255.0), 0.1f, 0.9f, 0, 40.0f);
-    auto pShaderFloor = std::make_shared<CShaderPhong>(scene, RGB(1, 1, 1), 0.1f, 0.9f, 0.0f, 40.0f);
+    auto pDarkBlue = std::make_shared<CShaderPhong>(scene, RGB(47 / 255.0, 60 / 255.0, 126 / 255.0), 0.3f, 0.9f, 0.7f, 40.0f);
+    auto pSunsetOrange = std::make_shared<CShaderPhong>(scene, RGB(251 / 255.0, 234 / 255.0, 235 / 255.0), 0.1f, 0.9f, 0.6f, 40.0f);
+    auto pShaderFloor = std::make_shared<CShaderPhong>(scene, RGB(1, 1, 1), 0.5f, 0.9f, 0.1f, 40.0f);
 
-    auto pShaderBlue = std::make_shared<CShaderEyelight>(RGB(240.0/ 255.0, 240.0/255.0, 230.0/255.0));
-    auto pShaderOrange = std::make_shared<CShaderEyelight>((217.0/ 255.0, 4.0/255.0, 41/255.0));
-
+    auto eyelight = std::make_shared<CShaderEyelight>(RGB(251 / 255.0, 234 / 255.0, 235 / 255.0));
 
     // Geometries
-    auto solidSphere1 = CSolidSphere(pShaderBlue, Vec3f(1, 0.1f, -13), 1.3f, 30, false);
-    auto solidSphere2 = CSolidSphere(pShaderBlue, Vec3f(1, 0.1f, -12), 1.0f, 30, false);
-    auto solidBox = CSolidBox(pShaderOrange, Vec3f(Vec3f(1, 0.1f, -13)), 2, 2, 2);
-    auto pComposize	 = std::make_shared<CCompositeGeometry>(solidBox, solidSphere1, BoolOp::Difference);
+    ptr_prim_t primSphere = std::make_shared<CPrimSphere>(pSunsetOrange, Vec3f(0, 0.0f, -10), 1.0f);
+    auto solidBox = CSolidBox(pDarkBlue, Vec3f(0, 0.0f, -10), 2, 2, 2);
+    auto solidTorus = CSolidTorus(pSunsetOrange, Vec3f(0, 0.0f, -10), 1, 0.5, 10, false);
+    solidTorus.transform(CTransform().rotate(Vec3f(1, 0, 0), 90).get());
+    ptr_prim_t pComposize	 = std::make_shared<CCompositeGeometry>(solidBox, solidTorus, BoolOp::Difference);
     scene.add(pComposize);
+    float s = 50;
+    float elevation = -2.0f;
+    scene.add(CSolidQuad(pShaderFloor, Vec3f(-s, elevation, -s), Vec3f(-s, elevation, s), Vec3f(s, elevation, s), Vec3f(s, elevation, -s)));
+    //auto floor = std::make_shared<CPrimPlane>(pShaderFloor, Vec3f(0, elevation, 0), Vec3f(0, 1, 0));
 
     // cameras
-    auto targetCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-2, 3, -3), pComposize->getBoundingBox().getCenter(), Vec3f(0, 1, 0), 45.0f);
+    auto targetCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-2, 3, -5), pComposize->getBoundingBox().getCenter(), Vec3f(0, 1, 0), 45.0f);
     scene.add(targetCamera);
 
-    scene.buildAccelStructure(20, 2);
-    //pComposize->buildAccelStructure();
+    scene.buildAccelStructure(20, 3);
 
     // Light
-    auto pLight = std::make_shared<CLightOmni>(intensity * RGB(1.0f, 0.839f, 0.494f), Vec3f(100, 150.0f, 100), false);
+    auto pLight = std::make_shared<CLightOmni>(intensity * RGB(1.0f, 0.839f, 0.494f), Vec3f(150.0f, 150.0f, 150.0f), true);
 
     // Add everything to scene
     scene.add(pLight);
