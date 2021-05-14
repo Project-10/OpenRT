c8846a2a6b21db4b3ed49160fe3a728ff95690ee
fjolladedaj <dedajfjolla@gmail.com>
2021-05-14 08:36:38 +0200

    Add Demo Room

---
+++ demos/Demo Room.cpp
@@ -0,0 +1,56 @@
+#include "openrt.h"
+#include "core/timer.h"
+
+using namespace rt;
+
+int main(int argc, char* argv[])
+{
+    const Vec3f    bgColor = RGB(0.8f, 0.9f, 1.0f);
+    const Size resolution(800, 400);
+    CScene scene(bgColor);
+    
+    // matherials
+    auto pShaderFloor    = std::make_shared<CShaderPhong>(scene, RGB(1, 1, 1), 0.1f, 0.9f, 0.0f, 40.0f);
+
+    CTransform t;
+    CSolid room(pShaderFloor, dataPath + "room.obj");
+
+    scene.add(room);
+
+    scene.add(std::make_shared<CLightOmni>(Vec3f::all(1e3), Vec3f(-50, 100, 0), true));
+    scene.add(std::make_shared<CLightOmni>(Vec3f::all(5e5), Vec3f(1000, 100, 250), true));
+    scene.add(std::make_shared<CLightSky>(Vec3f::all(0.25f)));
+    
+    // camera
+    const float r = 10;
+    auto mainCam = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(0, 0, r), Vec3f(0, 0, 0), Vec3f(0, 1, 0), 60.0f);
+    scene.add(mainCam);
+
+    scene.add(std::make_shared<CCameraStereoscopic>(resolution, Vec3f(0, 60, 0), Vec3f(-1, 0, 0), Vec3f(0, 1, 0), 1, 1));
+    scene.add(std::make_shared<CCameraStereoscopic>(resolution, Vec3f(0, 60, 0), Vec3f(-1, 0, 0), Vec3f(0, 1, 0), 2, 1));
+
+    scene.buildAccelStructure(20, 3);
+    
+    scene.setActiveCamera(1);
+    
+    Timer::start("Rendering Left... ");
+    Mat img_left = scene.render(std::make_shared<CSamplerStratified>(4));
+    Timer::stop();
+    
+    imshow("Left", img_left);
+    
+    scene.setActiveCamera(2);
+    
+    Timer::start("Rendering Right... ");
+    Mat img_right = scene.render(std::make_shared<CSamplerStratified>(4));
+    Timer::stop();
+    
+    imshow("Right", img_right);
+    
+    imwrite("D:\\renders\\img_left.jpg", img_left);
+    imwrite("D:\\renders\\img_right.jpg", img_right);
+
+    waitKey();
+    
+    return 0;
+}
--- modules/core/Solid.cpp
+++ modules/core/Solid.cpp
@@ -55,12 +55,12 @@ namespace rt {
 					}
 					//std::cout << "Face: " << V << std::endl;
 					//std::cout << "Normal: " << N << std::endl;
-					//add(std::make_shared<CPrimTriangle>(pShader, vVertexes[V.val[0]], vVertexes[V.val[1]], vVertexes[V.val[2]]));
+					add(std::make_shared<CPrimTriangle>(pShader, vVertexes[V.val[0]], vVertexes[V.val[1]], vVertexes[V.val[2]]));
 					//add(std::make_shared<CPrimTriangleSmooth>(pShader,  vVertexes[V.val[0]], vVertexes[V.val[1]], vVertexes[V.val[2]],
 					//													vNormals[N.val[0]], vNormals[N.val[1]], vNormals[N.val[2]]));
-					add(std::make_shared<CPrimTriangle>(pShader, vVertexes[V.val[0]], vVertexes[V.val[1]], vVertexes[V.val[2]],
-																 vTextures[T.val[0]], vTextures[T.val[1]], vTextures[T.val[2]],
-																 vNormals[N.val[0]], vNormals[N.val[1]], vNormals[N.val[2]]));
+                    //add(std::make_shared<CPrimTriangle>(pShader, vVertexes[V.val[0]], vVertexes[V.val[1]], vVertexes[V.val[2]],
+					//											 vTextures[T.val[0]], vTextures[T.val[1]], vTextures[T.val[2]],
+					//											 vNormals[N.val[0]], vNormals[N.val[1]], vNormals[N.val[2]]));
 	
 				}
 				else if (line == "#") {}
