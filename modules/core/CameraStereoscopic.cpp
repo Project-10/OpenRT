5c08803cad98ce810ae28ae57e228c5d6e0364b3
fjolladedaj <dedajfjolla@gmail.com>
2021-05-14 08:36:14 +0200

    Fix InitRay function for CCameraStereoscopic

--- modules/core/CameraStereoscopic.cpp
+++ modules/core/CameraStereoscopic.cpp
@@ -1,28 +1,43 @@
 #include "CameraStereoscopic.h"
 #include "Ray.h"
 #include "macroses.h"
-#include "CameraEnvironmentTarget.h"
 
 namespace rt
 {
 void CCameraStereoscopic::InitRay(Ray& ray, int x, int y, const Vec2f& sample)
     {
         Size resolution = getResolution();
-        const Vec3f& direction = normalize(m_dir - m_pos);
-        const Vec3f& translation_vector = normalize(direction.cross(m_up));
+        int width = resolution.width;
+        int height = resolution.height;
         
-        if(m_eye == 1) {
-            const Vec3f& left_position = m_pos - .5f * m_IPD * translation_vector;
-            CCameraEnvironmentTarget *Left = new CCameraEnvironmentTarget(resolution, left_position, m_dir, m_up);
+        float theta = Pif * ((y + sample.val[1]) / height);
+        float phi   = Pif * (((x + sample.val[0]) / width) * 2 - 0.5f);
         
-            Left->InitRay(ray, x, y);
+        if (m_needUpdateAxes) {
+            m_zAxis = m_dir;
+            m_xAxis = normalize(m_zAxis.cross(m_up));
+            m_yAxis = normalize(m_zAxis.cross(m_xAxis));
+            m_needUpdateAxes = false;
         }
+        
+        RT_ASSERT_MSG(m_dir != m_up, "Direction vector is the same as up vector");
+        
+        Vec3f ort = normalize(m_up.cross(m_dir));
             
-        if(m_eye == 2) {
-            const Vec3f& right_position = m_pos + .5f * m_IPD * translation_vector;
-            CCameraEnvironmentTarget *Right = new CCameraEnvironmentTarget(resolution, right_position, m_dir, m_up);
-        
-            Right->InitRay(ray, x, y);
+        // Left eye
+        if(m_eye == 1) {
+            ray.org = m_pos - (m_IPD / 2 * ort);
         }
+        
+        // Right eye
+        if(m_eye == 2) {
+            ray.org = m_pos + (m_IPD / 2 * ort);
+        }
+        
+        ray.dir = normalize(sinf(theta) * cosf(phi) * m_xAxis + sinf(theta) * sinf(phi) * m_zAxis - cosf(theta) * m_yAxis);
+        
+        ray.t = std::numeric_limits<double>::infinity();
+        ray.hit = nullptr;
     }
 }
+
