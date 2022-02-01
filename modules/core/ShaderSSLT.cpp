1ca24d083f05750476ea7e546f56fa33beced92e
tretyakovalex <60971868+tretyakovalex@users.noreply.github.com>
2022-02-01 20:16:39 +0100

    Added attenuation
    Called the coefficient of light attenuation function.

--- modules/core/ShaderSSLT.cpp
+++ modules/core/ShaderSSLT.cpp
@@ -11,16 +11,16 @@ namespace rt
 	}
 
 	Vec3f CShaderSSLT::shade(const Ray& ray) const
-        {
-            Vec3f n = ray.hit->getNormal(ray);
-            
-            Ray I(ray.hitPoint(), ray.dir, ray.counter);
-            Vec3f res = I.reTrace(m_scene);
-            
-            if (ray.dir.dot(n) < 0) { // entering the surface
-                const double k = 0.1;
-                const double alpha = MIN(1, sqrt(k * I.t));
-                return alpha * getDiffuseColor(ray) + (1 - alpha) * res;
-            } else return res;
-        }
+    	{
+		Vec3f n = ray.hit->getNormal(ray);
+
+		Ray I(ray.hitPoint(), ray.dir, ray.counter);
+		Vec3f res = I.reTrace(m_scene);
+
+		if (ray.dir.dot(n) < 0) { // entering the surface
+		    double attenuation = I.attenuation();
+		    const double alpha = MIN(1, sqrt(attenuation * I.t));
+		    return alpha * getDiffuseColor(ray) + (1 - alpha) * res;
+		} else return res;
+    	}
 }
