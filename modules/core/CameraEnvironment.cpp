50c3d52de752fa66d920a4373aea4a2e0cd42936
Sergey Kosov <sergey.kosov@project-10.de>
2022-03-09 14:41:28 +0100

    Bug Fix: The camera coordinate system
    Bug led to the effect of mirror representation of the scene

--- modules/core/CameraEnvironment.cpp
+++ modules/core/CameraEnvironment.cpp
@@ -14,18 +14,18 @@ namespace rt
         RT_IF_WARNING(x >= width, "Argument x = %d exceeds the camera resolution width (%d)", x, width);
         RT_IF_WARNING(y >= height, "Argument y = %d exceeds the camera resolution height (%d)", y, height);
         
-        float theta = Pif * ((y + sample.val[1]) / height);
-        float phi   = Pif * (((x + sample.val[0]) / width) * 2 - 0.5f);
-        
         if (m_needUpdateAxes) {
             m_zAxis = m_dir;
             m_xAxis = normalize(m_zAxis.cross(m_up));
             m_yAxis = normalize(m_zAxis.cross(m_xAxis));
             m_needUpdateAxes = false;
         }
+
+		float phi = 2 * Pif * (x + sample.val[0]) / width - Pif;
+		float theta = Pif * (y + sample.val[1]) / height;
         
         ray.org = m_pos;
-        ray.dir = normalize(sinf(theta) * cosf(phi) * m_xAxis + sinf(theta) * sinf(phi) * m_zAxis - cosf(theta) * m_yAxis);
+        ray.dir = normalize(sinf(theta) * cosf(phi) * m_zAxis + sinf(theta) * sinf(phi) * m_xAxis - cosf(theta) * m_yAxis);
         ray.t = std::numeric_limits<double>::infinity();
         ray.hit = nullptr;
     }
