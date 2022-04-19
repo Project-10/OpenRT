ef3feb878481f8aee1bb754b9a45e1f0755db7eb
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2022-04-19 20:01:50 +0200

    Update PrimSphere.h

--- modules/core/PrimSphere.h
+++ modules/core/PrimSphere.h
@@ -5,43 +5,43 @@
 #include "IPrim.h"
 
 namespace rt {
-        // ================================ Sphere Primitive Class ================================
-        /**
-         * @brief Sphere Geaometrical Primitive class
-         * @ingroup modulePrimitive
-         * @author Sergey G. Kosov, sergey.kosov@project-10.de
-         */
-        class CPrimSphere : public IPrim
-        {
-        public:
-            /**
-             * @brief Constructor
-             * @param pShader Pointer to the shader to be applied for the primitive
-             * @param origin Position of the center of the sphere
-             * @param radius Radius of the sphere
-             */
-            DllExport CPrimSphere(const ptr_shader_t pShader, const Vec3f& origin, float radius)
-                : IPrim(pShader)
-                , m_origin(origin)
-                , m_radius(radius)
-            {}
-            DllExport virtual ~CPrimSphere(void) = default;
+	// ================================ Sphere Primitive Class ================================
+	/**
+	 * @brief Sphere Geaometrical Primitive class
+	 * @ingroup modulePrimitive
+	 * @author Sergey G. Kosov, sergey.kosov@project-10.de
+	 */
+	class CPrimSphere : public IPrim
+	{
+	public:
+		/**
+		 * @brief Constructor
+		 * @param pShader Pointer to the shader to be applied for the primitive
+		 * @param origin Position of the center of the sphere
+		 * @param radius Radius of the sphere
+		 */
+		DllExport CPrimSphere(const ptr_shader_t pShader, const Vec3f& origin, float radius)
+			: IPrim(pShader)
+			, m_origin(origin)
+			, m_radius(radius)
+		{}
+		DllExport virtual ~CPrimSphere(void) = default;
 
-            DllExport virtual bool             intersect(Ray& ray) const override;
-            DllExport virtual bool             if_intersect(const Ray& ray) const override;
-            DllExport virtual void             transform(const Mat& T) override;
-            DllExport virtual Vec3f            getOrigin(void) const override { return m_origin; }
-            DllExport virtual Vec2f            getTextureCoords(const Ray& ray) const override;
-            DllExport virtual Vec3f         getObjectCoords(const Ray& ray) const override;
-            DllExport virtual CBoundingBox    getBoundingBox(void) const override;
+		DllExport virtual bool 			intersect(Ray& ray) const override;
+		DllExport virtual bool 			if_intersect(const Ray& ray) const override;
+		DllExport virtual void 			transform(const Mat& T) override;
+		DllExport virtual Vec3f			getOrigin(void) const override { return m_origin; }
+		DllExport virtual Vec2f			getTextureCoords(const Ray& ray) const override;
+                DllExport virtual Vec3f                 getObjectCoords(const );
+		DllExport virtual CBoundingBox	getBoundingBox(void) const override;
 
-        
-        private:
-            DllExport virtual Vec3f         doGetNormal(const Ray& ray) const override;
+	
+	private:
+		DllExport virtual Vec3f 		doGetNormal(const Ray& ray) const override;
 
-            
-        private:
-            Vec3f m_origin;        ///< Position of the center of the sphere
-            float m_radius;        ///< Radius of the sphere
-        };
+		
+	private:
+		Vec3f m_origin;		///< Position of the center of the sphere
+		float m_radius;		///< Radius of the sphere
+	};
 }
