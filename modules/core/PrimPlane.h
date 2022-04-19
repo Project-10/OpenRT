367df6a7a087091b208888f65a048bf62c018042
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2022-04-19 22:06:21 +0200

    Update PrimPlane.h

--- modules/core/PrimPlane.h
+++ modules/core/PrimPlane.h
@@ -1,3 +1,4 @@
+
 // Plane Geaometrical Primitive class
 // Written by Sergey Kosov in 2005 for Rendering Competition
 #pragma once
@@ -5,44 +6,44 @@
 #include "IPrim.h"
 
 namespace rt {
-        // ================================ Infinite Plane Primitive Class ================================
-        /**
-         * @brief The Plane Geometrical Primitive class
-         * @ingroup modulePrimitive
-         * @author Sergey G. Kosov, sergey.kosov@project-10.de
-         */
-        class CPrimPlane : public IPrim
-        {
-        public:
-            /**
-             * @brief Constructor
-             * @param pShader Pointer to the shader to be applied for the prim
-             * @param origin Point on the plane
-             * @param normal Normal to the plane
-             */
-            DllExport CPrimPlane(const ptr_shader_t pShader, const Vec3f& origin, const Vec3f& normal)
-                : IPrim(pShader)
-                , m_normal(normal)
-                , m_origin(origin)
-            {
-                normalize(m_normal);
-            }
-            DllExport virtual ~CPrimPlane(void) = default;
+	// ================================ Infinite Plane Primitive Class ================================
+	/**
+	 * @brief The Plane Geometrical Primitive class
+	 * @ingroup modulePrimitive
+	 * @author Sergey G. Kosov, sergey.kosov@project-10.de
+	 */
+	class CPrimPlane : public IPrim
+	{
+	public:
+		/**
+		 * @brief Constructor
+		 * @param pShader Pointer to the shader to be applied for the prim
+		 * @param origin Point on the plane
+		 * @param normal Normal to the plane
+		 */
+		DllExport CPrimPlane(const ptr_shader_t pShader, const Vec3f& origin, const Vec3f& normal)
+			: IPrim(pShader)
+			, m_normal(normal)
+			, m_origin(origin)
+		{
+			normalize(m_normal);
+		}
+		DllExport virtual ~CPrimPlane(void) = default;
 
-            DllExport virtual bool             intersect(Ray& ray) const override;
-            DllExport virtual bool             if_intersect(const Ray& ray) const override;
-            DllExport virtual void             transform(const Mat& T) override;
-            DllExport virtual Vec3f            getOrigin(void) const override { return m_origin; }
-            DllExport virtual Vec2f            getTextureCoords(const Ray& ray) const override;
-            DllExport virtual CBoundingBox    getBoundingBox(void) const override;
+		DllExport virtual bool 			intersect(Ray& ray) const override;
+		DllExport virtual bool 			if_intersect(const Ray& ray) const override;
+		DllExport virtual void 			transform(const Mat& T) override;
+		DllExport virtual Vec3f			getOrigin(void) const override { return m_origin; }
+		DllExport virtual Vec2f			getTextureCoords(const Ray& ray) const override;
+		DllExport virtual CBoundingBox	getBoundingBox(void) const override;
 
-            
-        private:
-            DllExport virtual Vec3f         doGetNormal(const Ray&) const override { return m_normal; }
-            
-            
-        private:
-            Vec3f m_normal;    ///< Point on the plane
-            Vec3f m_origin;    ///< Normal to the plane
-        };
+		
+	private:
+		DllExport virtual Vec3f 		doGetNormal(const Ray&) const override { return m_normal; }
+		
+		
+	private:
+		Vec3f m_normal;	///< Point on the plane
+		Vec3f m_origin;	///< Normal to the plane
+	};
 }
