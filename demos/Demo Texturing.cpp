b640f34e54c9d33402ecc3d888169172980e088a
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2022-06-05 20:11:10 +0200

    DemoTexturing update

--- demos/Demo Texturing.cpp
+++ demos/Demo Texturing.cpp
@@ -4,93 +4,169 @@
 
 using namespace rt;
 
-int main() {
-    const Vec3f		bgColor = RGB(0.1f, 0.1f, 0.1f);
-    const Size		resolution = Size(800, 600);
-    const float		intensity = 5000;
-
-    CScene scene(bgColor);
-    CScene scene1(bgColor);
-    CScene scene2(bgColor);
-    CScene scene3(bgColor);
-    
-    // Textures
-	auto pTextureRings	 = std::make_shared<CTextureRings>(1.0f);
+std::shared_ptr<CScene> buildSceneStripes(const Vec3f&bgColor, const Size resolution) {
+     
+     const float		intensity = 5000;
+     auto pScene  = std::make_shared<CScene>(bgColor);
+     
+     //textures
 	auto pTextureStripes = std::make_shared<CTextureStripes>(5);
-     auto pTextureWood = std::make_shared<CTextureWood>(1.5f);
-     auto pTextureMarble = std::make_shared<CTextureMarble>(1.0f);
-
-	// Shaders
-	auto pShaderFloor	= std::make_shared<CShaderPhong>(scene, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
- 	auto pShaderFloor1	= std::make_shared<CShaderPhong>(scene1, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
-   	auto pShaderFloor2	= std::make_shared<CShaderPhong>(scene2, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
-   	auto pShaderFloor3	= std::make_shared<CShaderPhong>(scene3, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
-     auto pShaderStripes	= std::make_shared<CShaderPhong>(scene, pTextureStripes, 0.1f, 0.9f, 0.0f, 40.0f);
-     auto pShaderWood	= std::make_shared<CShaderPhong>(scene1, pTextureWood, 0.1f, 0.9f, 0.0f, 200.0f);
-     auto pShaderMarble	= std::make_shared<CShaderPhong>(scene2, pTextureMarble, 0.1f, 0.9f, 0.0f, 200.0f);
-	auto pShaderRings	= std::make_shared<CShaderPhong>(scene3, pTextureRings, 0.1f, 0.9f, 0.0f, 40.0f);
-
-
-    
-    
-	// Geometry
-	scene.add(CSolidQuad(pShaderFloor, Vec3f::all(0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
-     scene1.add(CSolidQuad(pShaderFloor1, Vec3f(0,-10,-0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
-     scene2.add(CSolidQuad(pShaderFloor2, Vec3f(0,-10,-0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
-     scene3.add(CSolidQuad(pShaderFloor3, Vec3f::all(0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
-
-
+     
+     //Shaders
+     auto pShaderStripes	= std::make_shared<CShaderPhong>(*pScene, pTextureStripes, 0.1f, 0.9f, 0.0f, 40.0f);
+	auto pShaderFloor	= std::make_shared<CShaderPhong>(*pScene, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
+ 
+     //Geometries
+	pScene->add(CSolidQuad(pShaderFloor, Vec3f::all(0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
 	CSolid teapot(pShaderStripes, dataPath + "teapot.obj");
-     //CSolid teapot1(pShaderMarble, dataPath + "teapot.obj");
-     auto solidSphere2 = CSolidSphere(pShaderWood , Vec3f(0,0,0) ,13);
-     auto solidBox = CSolidBox(pShaderMarble , Vec3f(0,0,0) , 9);
-     CSolid teapot3(pShaderRings , dataPath + "teapot.obj");
-     scene1.add(solidSphere2);
-     scene2.add(solidBox);
-    CTransform t;
+
+     //Transformation
+     CTransform t;
 	teapot.transform(t.scale(5.0f).get());
- 	teapot3.transform(t.scale(5.5f).translate(0,-2,0).get());
-     scene3.add(teapot3);
-    scene.add(teapot);
-    
+     pScene->add(teapot);
+     
+     //Light
+     auto pLightPoint = std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, 100, -10));
+     auto pLightSpot = std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(20, 100, -10), Vec3f(0, 0, 0), 15.0f, 30.0f);
+     if (false) pScene->add(pLightPoint);
+     else pScene->add(pLightSpot);
+     
+     
+     //Cameras
     auto pCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-15, 30, 40), Vec3f(0, 5, 0), Vec3f(0, 1, 0), 45.0f);
-    auto pLightPoint = std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, 100, -10));
-    auto pLightSpot = std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(20, 100, -10), Vec3f(0, 0, 0), 15.0f, 30.0f);
-    scene.add(pCamera);
-    scene1.add(pCamera);
-    scene2.add(pCamera);
-    scene3.add(pCamera);
-    if (false) scene.add(pLightPoint);
-    else scene.add(pLightSpot);
-    if (false) scene1.add(pLightPoint);
-    else scene1.add(pLightSpot);
-     if (false) scene2.add(pLightPoint);
-    else scene2.add(pLightSpot);
-     if (false) scene3.add(pLightPoint);
-    else scene3.add(pLightSpot);
-
-    scene.buildAccelStructure(25, 5);
-    scene1.buildAccelStructure(25,5);
-    scene2.buildAccelStructure(25,5);
-    scene3.buildAccelStructure(25,5);
-
-   int x = 312;
-   x  = x & 2;
-   std::cout << x << std::endl;
+    pScene->add(pCamera);
     
-    Timer::start("Rendering...");
-    Mat img = scene.render(std::make_shared<CSamplerStratified>(2, true, true));
-    Mat img1 = scene1.render(std::make_shared<CSamplerStratified>(2, true, true));
-    Mat img2 = scene2.render(std::make_shared<CSamplerStratified>(2, true, true));
-    Mat img3 = scene3.render(std::make_shared<CSamplerStratified>(2, true, true));
-
-    Timer::stop();
-    imshow("Stripes Texture", img);
-    imshow("Wood Texture", img1);
-    imshow("Marble Texture", img2);
-    imshow("Rings Texture", img3);
-    waitKey();
-    return 0;
+    return pScene;
 }
 
 
+std::shared_ptr<CScene> buildSceneRings(const Vec3f&bgColor, const Size resolution) {
+     
+     const float		intensity = 5000;
+     auto pScene  = std::make_shared<CScene>(bgColor);
+     
+     //textures
+	auto pTextureRings = std::make_shared<CTextureRings>(1.5f);
+     
+     //Shaders
+     auto pShaderRings	= std::make_shared<CShaderPhong>(*pScene, pTextureRings, 0.1f, 0.9f, 0.0f, 40.0f);
+	auto pShaderFloor	= std::make_shared<CShaderPhong>(*pScene, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
+ 
+     //Geometries
+	pScene->add(CSolidQuad(pShaderFloor, Vec3f::all(0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
+	CSolid teapot(pShaderRings, dataPath + "teapot.obj");
+
+     //Transformation
+     CTransform t;
+	teapot.transform(t.scale(5.0f).get());
+     pScene->add(teapot);
+     
+     //Light
+     auto pLightPoint = std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, 100, -10));
+     auto pLightSpot = std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(20, 100, -10), Vec3f(0, 0, 0), 15.0f, 30.0f);
+     if (false) pScene->add(pLightPoint);
+     else pScene->add(pLightSpot);
+     
+     
+     //Cameras
+    auto pCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-15, 30, 40), Vec3f(0, 5, 0), Vec3f(0, 1, 0), 45.0f);
+    pScene->add(pCamera);
+    
+    return pScene;
+}
+
+std::shared_ptr<CScene> buildSceneWood(const Vec3f&bgColor, const Size resolution) {
+     
+     const float		intensity = 5000;
+     auto pScene  = std::make_shared<CScene>(bgColor);
+     
+     //textures
+	auto pTextureWood = std::make_shared<CTextureWood>(1.5f);
+     
+     //Shaders
+     auto pShaderWood	= std::make_shared<CShaderPhong>(*pScene, pTextureWood, 0.1f, 0.9f, 0.0f, 40.0f);
+	auto pShaderFloor	= std::make_shared<CShaderPhong>(*pScene, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
+ 
+     //Geometries
+	pScene->add(CSolidQuad(pShaderFloor, Vec3f(0,-10,0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
+     auto solidSphere = CSolidSphere(pShaderWood , Vec3f(0,0,0) ,13);
+     pScene->add(solidSphere);
+     
+     //Light
+     auto pLightPoint = std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, 100, -10));
+     auto pLightSpot = std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(20, 100, -10), Vec3f(0, 0, 0), 15.0f, 30.0f);
+     if (false) pScene->add(pLightPoint);
+     else pScene->add(pLightSpot);
+     
+     
+     //Cameras
+    auto pCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-15, 30, 40), Vec3f(0, 5, 0), Vec3f(0, 1, 0), 45.0f);
+    pScene->add(pCamera);
+    
+    return pScene;
+}
+
+
+std::shared_ptr<CScene> buildSceneMarble(const Vec3f&bgColor, const Size resolution) {
+     
+     const float		intensity = 5000;
+     auto pScene  = std::make_shared<CScene>(bgColor);
+     
+     //textures
+	auto pTextureMarble = std::make_shared<CTextureMarble>(1.0f);
+     
+     //Shaders
+     auto pShaderMarble	= std::make_shared<CShaderPhong>(*pScene, pTextureMarble, 0.1f, 0.9f, 0.0f, 40.0f);
+	auto pShaderFloor	= std::make_shared<CShaderPhong>(*pScene, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
+ 
+     //Geometries
+	pScene->add(CSolidQuad(pShaderFloor, Vec3f(0,-10,0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
+     auto solidBox = CSolidBox(pShaderMarble , Vec3f(0,0,0) ,9);
+     pScene->add(solidBox);
+     
+     //Light
+     auto pLightPoint = std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, 100, -10));
+     auto pLightSpot = std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(20, 100, -10), Vec3f(0, 0, 0), 15.0f, 30.0f);
+     if (false) pScene->add(pLightPoint);
+     else pScene->add(pLightSpot);
+     
+     
+     //Cameras
+    auto pCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-15, 30, 40), Vec3f(0, 5, 0), Vec3f(0, 1, 0), 45.0f);
+    pScene->add(pCamera);
+    
+    return pScene;
+}
+
+
+int main() {
+
+     const Vec3f		bgColor = RGB(0.1f, 0.1f, 0.1f);
+    const Size		     resolution = Size(800, 600);
+    
+     auto pSceneStripes = buildSceneStripes(bgColor, resolution);
+     auto pSceneRings = buildSceneRings(bgColor, resolution);
+     auto pSceneWood = buildSceneWood(bgColor, resolution);
+     auto pSceneMarble = buildSceneMarble(bgColor, resolution);
+     
+     pSceneStripes->buildAccelStructure(25,5);
+     pSceneRings-> buildAccelStructure(25,5);
+     pSceneWood-> buildAccelStructure(25,5);
+     pSceneMarble-> buildAccelStructure(25,5);
+
+     
+     Timer::start("Rendering...");
+     Mat imgStripes = pSceneStripes->render(std::make_shared<CSamplerStratified>(2, true, true));
+     Mat imgRings = pSceneRings->render(std::make_shared<CSamplerStratified>(2, true, true));
+     Mat imgWood = pSceneWood->render(std::make_shared<CSamplerStratified>(2, true, true));
+     Mat imgMarble = pSceneMarble->render(std::make_shared<CSamplerStratified>(2, true, true));
+
+
+     Timer::stop();
+     imshow("Stripes Texture", imgStripes);
+     imshow("Rings Texture", imgRings);
+     imshow("Wood Texture", imgWood);
+     imshow("Marble Texture", imgMarble);
+     waitKey();
+     return 0;
+}
