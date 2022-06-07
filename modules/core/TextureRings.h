49b876492499425819b583503a627fd50bb73887
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2022-06-07 13:49:42 +0200

    Update TextureRings.h

--- modules/core/TextureRings.h
+++ modules/core/TextureRings.h
@@ -5,19 +5,6 @@
 #include"Texture.h"
 
 namespace rt{
-//	class CTextureRings : public CTexture{
-//		public:
-//		DllExport CTextureRings(float period) : m_period(period) {}
-//		DllExport virtual ~CTextureRings(void) = default;
-//    
-//
-//		DllExport Vec3f getTexel(const Vec3f &uvw) const override;
-//    
-//		DllExport bool isProcedural(void) const override { return true; }
-//
-//
-//	private:
-//		float m_period;
   
      class CTextureRings : public CTexture{
      
