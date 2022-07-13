9cfb1c720a6725113c289587338f54a54f5d7f22
Sergey Kosov <sergey.kosov@project-10.de>
2022-07-13 22:52:39 +0200

    Remove redundant part of the permutation vector

--- modules/core/PerlinNoise.cpp
+++ modules/core/PerlinNoise.cpp
@@ -9,53 +9,53 @@
 
 
 namespace rt{
-namespace
-{
-/**  @Brief Smoothstep function
- @param val value that we want to use the smoothstep on
- @return smoothstep value
- */
-float fade(float val)
-{
-     return val * val * val * (val * (val * 6 - 15) + 10);
-}
+	namespace
+	{
+		/**  @Brief Smoothstep function
+		 @param val value that we want to use the smoothstep on
+		 @return smoothstep value
+		 */
+		float fade(float val)
+		{
+			 return val * val * val * (val * (val * 6 - 15) + 10);
+		}
 
-/**
- @Brief Interpolation function
- @param val value that we want to interpolated
- @param a first border of the interpolation
- @param b second border of the interpolation
- @return interpolated value
- */
-float lerp(float val, float a, float b)
-{
-     return a + val * (b - a);
-}
+		/**
+		 @Brief Interpolation function
+		 @param val value that we want to interpolated
+		 @param a first border of the interpolation
+		 @param b second border of the interpolation
+		 @return interpolated value
+		 */
+		float lerp(float val, float a, float b)
+		{
+			 return a + val * (b - a);
+		}
 
-//Calculate the gradiant
-/**
- @Brief Gradient function
- @param hash hashing value
- @param x x-coordinate value
- @param y y-coordinate value
- @param z z-coordinate value
- */
-float grad(int hash, float x, float y, float z)
-{
-     //Converts the lower 4 bits into 12 gradient directions
-     int h = hash & 15;
+		//Calculate the gradiant
+		/**
+		 @Brief Gradient function
+		 @param hash hashing value
+		 @param x x-coordinate value
+		 @param y y-coordinate value
+		 @param z z-coordinate value
+		 */
+		float grad(int hash, float x, float y, float z)
+		{
+			 //Converts the lower 4 bits into 12 gradient directions
+			 int h = hash & 15;
      
-     float u = h < 8 ? x : y;
-     float v = h < 4 ? y : h == 12 || h == 14 ? x : z;
-     return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
-}
-}
+			 float u = h < 8 ? x : y;
+			 float v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+			 return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+		}
+	}
 
 
 float CPerlinNoise::noise(const Point3f& p)
 {
-     //Find the unit cube that contains the point
-     int X  = (int) floor(p.x) & 255;
+	 //Find the unit cube that contains the point
+     int X = (int) floor(p.x) & 255;
      int Y = (int) floor(p.y) & 255;
      int Z = (int) floor(p.z) & 255;
      
@@ -70,22 +70,26 @@ float CPerlinNoise::noise(const Point3f& p)
      float w = fade(z);
      
      //Hash coordinates of 8 cube corners
-     int A = m_p[X] + Y;
-     int AA = m_p[A] + Z;
-     int AB = m_p[A + 1] + Z;
-     int B = m_p[X + 1] + Y;
-     int BA = m_p[B] + Z;
-     int BB = m_p[B + 1] + Z;
+     const int n = static_cast<int>(m_p.size());
+	 int A  = (m_p[X % n] + Y);
+	 int B  = (m_p[(X + 1) % n] + Y);
      
-     //Interpolation
-     return lerp(w ,
-                 lerp(v,
-                      lerp(u, grad(m_p[AA], x, y, z), grad(m_p[BA], x-1, y, z)),
-                      lerp(u, grad(m_p[AB], x, y-1, z), grad(m_p[BB], x-1, y-1, z))),
-                 lerp(v ,
-                      lerp(u, grad(m_p[AA+1], x, y, z-1), grad(m_p[BA+1], x-1, y, z-1)),
-                      lerp(u, grad(m_p[AB+1], x, y-1, z-1), grad(m_p[BB+1], x-1, y-1, z-1)))
-                 );
+	 int AA = (m_p[A % n] + Z);
+     int AB = (m_p[(A + 1) % n] + Z);
+     int BA = (m_p[B % n] + Z);
+     int BB = (m_p[(B + 1) % n] + Z);
+
+     // Interpolation
+     return	lerp(w ,
+				lerp(v,
+					lerp(u, grad(m_p[AA % n], x, y, z),   grad(m_p[BA % n], x-1, y, z)),
+					lerp(u, grad(m_p[AB % n], x, y-1, z), grad(m_p[BB % n], x-1, y-1, z))
+				),
+				lerp(v ,
+					lerp(u, grad(m_p[(AA+1) % n], x, y, z-1),   grad(m_p[(BA+1) % n], x-1, y, z-1)),
+					lerp(u, grad(m_p[(AB+1) % n], x, y-1, z-1), grad(m_p[(BB+1) % n], x-1, y-1, z-1))
+				)
+			);
 }
 
 float CPerlinNoise::turbulence(const Point3f& p, int octaves, float f, float amplitude, float lacunarity ,float persistence)  
