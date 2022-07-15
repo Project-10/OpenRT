c3b07cdc2810c9c585a46d0a2f3a13d1c346e0a9
Sergey Kosov <sergey.kosov@project-10.de>
2022-07-15 16:37:10 +0200

    Refactoring CGradient class

--- modules/core/CGradient.cpp
+++ modules/core/CGradient.cpp
@@ -3,62 +3,57 @@
 #include "CGradient.h"
 #include "macroses.h"
 
-namespace rt{
+namespace rt {
 
-namespace{
+	// Constructor
+	CGradient::CGradient(const Vec3f& color0, const Vec3f& color1) 
+		: m_color0(color0)
+		, m_color1(color1)
+	{
+		//if (color1.getWeight() != 0 || color2.getWeight() != 1)
+		//	RT_ASSERT_MSG(false, "First and second elements should always have a weight value of 0 and 1 respectively");
+		//m_colorsVec.push_back(Element(color1));
+		//m_colorsVec.push_back(Element(color2));
+	};
 
-Vec3f mix(const Vec3f& C0, const Vec3f& C1, float f)
-{
-     return (1 - f) * C0 + f * C1;
-}
+
+	void CGradient::addColor(const Vec3f& color, float pos)
+	{
+		 //float weight = newColor.getWeight();
+		 //Vec3f color = newColor.getColor();
+		 //for (int it = 0; it < m_colorsVec.size(); it++) {
+			//  if (m_colorsVec[it].getWeight() < weight) {
+			//	   m_colorsVec.insert(m_colorsVec.begin() + it + 1, newColor);
+			//	   break;
+			//  }
+   //       
+			//  if(m_colorsVec[it].getWeight() == weight || m_colorsVec[it].getColor() == color){
+			//	   RT_ASSERT_MSG(false, "Weight or color already mentioned, please change");
+			//  }
+   //       
+		 //}
+	}
 
 
-}
+	Vec3f CGradient::getColor(float val) const
+	{
+		return (1 - val) * m_color0 + val * m_color1;
 
-float Element::getWeight(){
-     return this->m_weight;
-}
-
-Vec3f Element::getColor(){
-     return this->m_color;
-}
-
-//Add color in the vector considering its weight with respect to the other element's weights
-void CGradient::addColor(Element newColor){
-     float weight = newColor.getWeight();
-     Vec3f color = newColor.getColor();
-     for (int it = 0; it < m_colorsVec.size(); it++) {
-          if (m_colorsVec[it].getWeight() < weight) {
-               m_colorsVec.insert(m_colorsVec.begin() + it + 1, newColor);
-               break;
-          }
-          
-          if(m_colorsVec[it].getWeight() == weight || m_colorsVec[it].getColor() == color){
-               RT_ASSERT_MSG(false, "Weight or color already mentioned, please change");
-          }
-          
-     }
-}
-
-//Calculate color gradient
-Vec3f CGradient::getColorGradient(float val){
-     std::vector<Element>::iterator it;
-     
-     for (int it = 0; it < m_colorsVec.size(); it++) {
-          if (val > 1 || val < 0) {
-               RT_ASSERT_MSG(false, "Val problem");
-          }
-          if(val > m_colorsVec[it].getWeight()  && val < m_colorsVec[it+1].getWeight()){
-               return mix(m_colorsVec[it].getColor() , m_colorsVec[it+1].getColor() , val);
-          }
-          
-          if (val == 1) {
-               return m_colorsVec[it+m_colorsVec.size()-1].getColor();
-          }
-          
-          else return m_colorsVec[0].getColor();
-     }
-     
-     
-}
+		 //std::vector<Element>::iterator it;
+   //  
+		 //for (int it = 0; it < m_colorsVec.size(); it++) {
+			//  if (val > 1 || val < 0) {
+			//	   RT_ASSERT_MSG(false, "Val problem");
+			//  }
+			//  if(val > m_colorsVec[it].getWeight()  && val < m_colorsVec[it+1].getWeight()){
+			//	   return mix(m_colorsVec[it].getColor() , m_colorsVec[it+1].getColor() , val);
+			//  }
+   //       
+			//  if (val == 1) {
+			//	   return m_colorsVec[it+m_colorsVec.size()-1].getColor();
+			//  }
+   //       
+			//  else return m_colorsVec[0].getColor();
+		 //}
+	}
 }
--- modules/core/CGradient.h
+++ modules/core/CGradient.h
@@ -1,74 +1,36 @@
 //  Created by Mahmoud El Bergui on 12.07.22.
 
 #pragma once
-#include <stdio.h>
-#include "Texture.h"
-#include<list>
-#include<vector>
-#include"macroses.h"
+#pragma once
+
+#include "types.h"
 
 namespace rt{
-
-/**
- @Brief this small class is used to define the one instance of the m_colorsVec
- */
-class Element{
-public:
-     /**
-      @Brief Creates an instance with a color and its weight
-      */
-     DllExport Element(Vec3f color , float val): m_color(color) , m_weight(val){
-          if (m_weight > 1 || m_weight < 0 )
-               RT_ASSERT_MSG(false,"Weight should be between 0 and 1");
-     };
+	class CGradient{
+	public:
+		/**
+		* @brief Defines the borders of the color gradient
+		* @param color0
+		* @param color1
+		*/
+		DllExport CGradient(const Vec3f& color0, const Vec3f& color1);
      
-     /**
-      @Brief get the weight of the element
-      */
-     float getWeight();
+		/**
+		@Brief Adds a color and its weight to the gradient vector
+		@Details 0 <newcolor's weight<1 because 0 and 1 are already taken
+		*/
+		DllExport void addColor(const Vec3f& color, float pos);
      
-     /**
-      @Brief get the color of the element
-      */
-     Vec3f getColor();
-     
-private:
-     Vec3f m_color;
-     float m_weight;
-};
-
-
-class CGradient{
-public:
-     /**
-      @Brief Defines the borders of the color gradient
-      */
-     DllExport CGradient(Element color1 , Element color2 , float val):
-     m_color1(color1), m_color2(color2) , m_val(val)
-     {
-          if(color1.getWeight() != 0 || color2.getWeight() != 1)
-               RT_ASSERT_MSG(false, "First and second elements should always have a weight value of 0 and 1 respectively");
-          m_colorsVec.push_back(Element(color1));
-          m_colorsVec.push_back(Element(color2));
-     };
-     
-     /**
-      @Brief Adds a color and its weight to the gradient vector
-      @Details 0 <newcolor's weight<1 because 0 and 1 are already taken
-      */
-     DllExport void addColor(Element newcolor);
-     
-     /**
-      @Brief Calculates the gradient of val
-      */
-     DllExport Vec3f getColorGradient(float val);
+		/**
+		@Brief Calculates the gradient of val
+		*/
+		DllExport Vec3f getColor(float val) const;
      
      
-private:
-     Element m_color1;                                 //RGB color that has a weight value of 0
-     Element m_color2;                                 //RGB color that has a weight value of 1
-     float m_val;                                      //Value of the interpolation
-     std::vector<Element> m_colorsVec;                 //Vector that stores the RGB colors and their weight
+	private:
+		 Vec3f m_color0;                                 // RGB color that has a weight value of 0
+		 Vec3f m_color1;                                 // RGB color that has a weight value of 1
+	//     std::vector<Element> m_colorsVec;                 // Vector that stores the RGB colors and their weight
      
-};
+	};
 }
