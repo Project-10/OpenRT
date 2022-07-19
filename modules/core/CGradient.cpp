049b76dd1a7483fb7f3b9d890698cd7a854f9502
Sergey Kosov <sergey.kosov@project-10.de>
2022-07-19 22:32:32 +0200

    Implementation for CGradient class

--- modules/core/CGradient.cpp
+++ modules/core/CGradient.cpp
@@ -1,63 +1,19 @@
 //  Created by Mahmoud El Bergui on 12.07.22.
 
 #include "CGradient.h"
-#include "macroses.h"
 
 namespace rt {
-
-	// Constructor
-	CGradient::CGradient(const Vec3f& color0, const Vec3f& color1) 
-		: m_color0(color0)
-		, m_color1(color1)
-	{
-		//if (color1.getWeight() != 0 || color2.getWeight() != 1)
-		//	RT_ASSERT_MSG(false, "First and second elements should always have a weight value of 0 and 1 respectively");
-		//m_colorsVec.push_back(Element(color1));
-		//m_colorsVec.push_back(Element(color2));
-	};
-
-
-	void CGradient::addColor(const Vec3f& color, float pos)
-	{
-		 //float weight = newColor.getWeight();
-		 //Vec3f color = newColor.getColor();
-		 //for (int it = 0; it < m_colorsVec.size(); it++) {
-			//  if (m_colorsVec[it].getWeight() < weight) {
-			//	   m_colorsVec.insert(m_colorsVec.begin() + it + 1, newColor);
-			//	   break;
-			//  }
-   //       
-			//  if(m_colorsVec[it].getWeight() == weight || m_colorsVec[it].getColor() == color){
-			//	   RT_ASSERT_MSG(false, "Weight or color already mentioned, please change");
-			//  }
-   //       
-		 //}
-	}
-
-
-	Vec3f CGradient::getColor(float val) const
+	Vec3f CGradient::getColor(float x) const
 	{
 		// These 2 lines are for debugging
-		if (val < 0) return RGB(0, 0, 255);
-		if (val > 1) return RGB(255, 0, 0);
+		if (x < 0) return RGB(0, 0, 255);
+		if (x > 1) return RGB(255, 0, 0);
 		
-		return (1 - val) * m_color0 + val * m_color1;
+		auto b = m_mColors.lower_bound(x);		// upper known position
+		auto a = std::prev(b);					// lower known position
+		
+		float y = (x - a->first) / (b->first - a->first); // y \in [0, 1] to interpolate between known positions
 
-		 //std::vector<Element>::iterator it;
-   //  
-		 //for (int it = 0; it < m_colorsVec.size(); it++) {
-			//  if (val > 1 || val < 0) {
-			//	   RT_ASSERT_MSG(false, "Val problem");
-			//  }
-			//  if(val > m_colorsVec[it].getWeight()  && val < m_colorsVec[it+1].getWeight()){
-			//	   return mix(m_colorsVec[it].getColor() , m_colorsVec[it+1].getColor() , val);
-			//  }
-   //       
-			//  if (val == 1) {
-			//	   return m_colorsVec[it+m_colorsVec.size()-1].getColor();
-			//  }
-   //       
-			//  else return m_colorsVec[0].getColor();
-		 //}
+		return (1 - y) * a->second + y * b->second;	// interpolating between 2 colors
 	}
 }
--- modules/core/CGradient.h
+++ modules/core/CGradient.h
@@ -6,32 +6,43 @@
 #include "types.h"
 
 namespace rt{
-	// TODO: copy constructor and copy operator
+	/**
+	 * brief Gradient class interpolates between the given golors in range [0; 1]
+	 */
 	class CGradient{
 	public:
 		/**
-		* @brief Defines the borders of the color gradient
-		* @param color0
-		* @param color1
+		* @brief Constructor
+		* @param color0 Color value for the point 0
+		* @param color1 Color point for the point 1
 		*/
-		DllExport CGradient(const Vec3f& color0 = Vec3f::all(0), const Vec3f& color1 = Vec3f::all(1));
+		DllExport CGradient(const Vec3f& color0 = Vec3f::all(0), const Vec3f& color1 = Vec3f::all(1)) : m_mColors({{0, color0}, {1, color1}}) {}
+		/**
+		* @brief Constructor
+		* @param colors The map with colors and their positions
+		* @warning Take care to assign a color for points 0 and 1
+		*/
+		DllExport CGradient(const std::map<float, Vec3f>& colors) : m_mColors(colors) {}
+		DllExport CGradient(const CGradient&) = delete;
+		DllExport ~CGradient(void) = default;
+		DllExport const CGradient& operator=(const CGradient&) = delete;
      
 		/**
-		@Brief Adds a color and its weight to the gradient vector
-		@Details 0 <newcolor's weight<1 because 0 and 1 are already taken
-		*/
-		DllExport void addColor(const Vec3f& color, float pos);
+		 * @brief Adds a color to gradient
+		 * @param pos The point at which the color should be added in range [0; 1]
+		 * @param color The color
+		 */
+		DllExport void addColor(float pos, const Vec3f& color) { m_mColors[pos] = color; }
      
 		/**
-		@Brief Calculates the gradient of val
-		*/
-		DllExport Vec3f getColor(float val) const;
+		 * @brief Returns the color at given point
+		 * @param pos The poiint from which the color value should be calculated and returnes
+		 * @returns The color value
+		 */
+		DllExport Vec3f getColor(float pos) const;
      
      
 	private:
-		 Vec3f m_color0;                                 // RGB color that has a weight value of 0
-		 Vec3f m_color1;                                 // RGB color that has a weight value of 1
-	//     std::vector<Element> m_colorsVec;                 // Vector that stores the RGB colors and their weight
-     
+		std::map<float, Vec3f>	m_mColors;	//< Data container that stores the RGB colors and their positions
 	};
 }
