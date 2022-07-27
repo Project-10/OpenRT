8e8d317fea9dece0ecfb2674c9dac0ae54f96e3c
Sergey Kosov <sergey.kosov@project-10.de>
2022-07-27 16:18:29 +0200

    Refactoring Rings procedural texture

--- modules/core/TextureRings.cpp
+++ modules/core/TextureRings.cpp
@@ -1,28 +1,16 @@
-//
-//  Rings.cpp
-//  Demo_AreaLight
-//
 //  Created by Mahmoud El Bergui on 18.04.22.
-//
 
 #include "TextureRings.h"
 
 namespace rt{
-
+	/// @todo Play with the direction of the rings
 	Vec3f CTextureRings::getTexel(const Vec3f& uvw) const
 	{
-		//Getting coordinate info
-		float u = uvw.val[0];
-		float v = uvw.val[1];
-		float w = uvw.val[2];
-    
+		const Vec3f period = m_period * Vec3f(1, 1, 0); // orintation of the rings
+		
 		//Difference between point and center of the shape
-		float value = m_period * sqrtf(w * w + u * u);
+		float value = static_cast<float>(norm(uvw.mul(period)));
 
 		return static_cast<int>(value) % 2 == 0 ? Vec3f(0, 0, 1) : Vec3f::all(1);
 	}
-
 }
-
-
-
