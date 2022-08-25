e9c479ab069ed62fb318d3e5467f1a93e04d4f65
Sergey Kosov <sergey.kosov@project-10.de>
2022-08-25 19:03:53 +0200

    Initial Implementation

---
+++ modules/core/ShaderShadow.cpp
@@ -0,0 +1,44 @@
+#include "ShaderShadow.h"
+#include "Scene.h"
+#include "Ray.h"
+
+namespace rt {
+	Vec3f CShaderShadow::shade(const Ray& ray) const
+	{
+		// Traverse ray behind the geometry to make it fully transparent
+		Ray R(ray.hitPoint(), ray.dir, ray.counter);	// traverse ray
+		Vec3f res = R.reTrace(m_scene);
+		
+		// Gathering shadows
+		Vec3f shadingNormal = ray.hit->getShadingNormal(ray);
+		
+		Ray I(ray.hitPoint(shadingNormal));				// shadow ray
+		Vec3f L_possible = Vec3f::all(0);
+		Vec3f L_actual = Vec3f::all(0);
+		for (auto& pLight : m_scene.getLights()) {
+			const size_t nSamples = pLight->getNumSamples();
+			const float avg = 1.0f / nSamples;
+			for (size_t s = 0; s < nSamples; s++) {
+				// get direction to light, and intensity
+				I.hit = ray.hit;	// TODO: double check
+				auto radiance = pLight->illuminate(I);
+				if (radiance) {
+					float cosLightNormal = I.dir.dot(shadingNormal);
+					if (cosLightNormal > 0) {
+						Vec3f L = avg * cosLightNormal * radiance.value();
+						L_possible += L;
+						if (!pLight->shadow() || !m_scene.if_intersect(I))
+							L_actual += L;
+					}
+				}
+			} // s
+		} // pLight
+
+		for (int i = 0; i < 3; i++) {
+			float l = L_possible[i] > 0 ? (L_possible[i] - L_actual[i]) / L_possible[i] : 0.0f;
+			res[i] *= (1.0f - l);
+		}
+
+		return res;
+	}
+}
