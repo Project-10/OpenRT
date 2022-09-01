66e90cff8236df83ac4400a81d8908fd1dcd050e
Sergey Kosov <sergey.kosov@project-10.de>
2022-09-01 23:11:17 +0200

    Add test for transforms

---
+++ tests/TestTransform.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "gtest/gtest.h"
+#include "types.h"
+#include "openrt.h"
+
+class CTestTransform : public ::testing::Test {
+public:
+	CTestTransform(void) = default;
+    ~CTestTransform(void) = default;
+};
