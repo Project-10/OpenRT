18435b04df22065bf3e711aa0eb645e27f6a394f
Sergey Kosov <ereator@mail.ru>
2022-09-02 14:42:19 +0200

    Refactoring Transform.cpp
    x.val[i] -> x[i]

--- modules/core/Transform.cpp
+++ modules/core/Transform.cpp
@@ -4,14 +4,14 @@ namespace rt {
 	CTransform CTransform::scale(const Vec3f& S) const {
 		Mat t = Mat::eye(4, 4, CV_32FC1);
 		for (int i = 0; i < 3; i++)
-			t.at<float>(i, i) = S.val[i];
+			t.at<float>(i, i) = S[i];
 		return CTransform(t * m_t);
 	}
 
 	CTransform CTransform::translate(const Vec3f& T) const {
 		Mat t = Mat::eye(4, 4, CV_32FC1);
 		for (int i = 0; i < 3; i++) 
-			t.at<float>(i, 3) = T.val[i];
+			t.at<float>(i, 3) = T[i];
 		return CTransform(t * m_t);
 	}
 
@@ -21,9 +21,9 @@ namespace rt {
 		theta *= Pif/180;
 		float cos_theta = cosf(theta);
 		float sin_theta = sinf(theta);
-		float x = k.val[0];
-		float y = k.val[1];
-		float z = k.val[2];
+		float x = k[0];
+		float y = k[1];
+		float z = k[2];
 		
 		t.at<float>(0, 0) = cos_theta + (1 - cos_theta) * x * x;
 		t.at<float>(0, 1) = (1 - cos_theta) * x * y - sin_theta * z;
@@ -41,14 +41,14 @@ namespace rt {
 	}
 
 	Vec3f CTransform::point(const Vec3f& p, const Mat& t) {
-		Vec4f P = Vec4f(p.val[0], p.val[1], p.val[2], 1);
+		Vec4f P = Vec4f(p[0], p[1], p[2], 1);
 		P = Vec4f(reinterpret_cast<float*>(Mat(t * Mat(P)).data));
-		return Vec3f(P.val[0], P.val[1], P.val[2]) / P.val[3];
+		return Vec3f(P[0], P[1], P[2]) / P[3];
 	}
 
 	Vec3f CTransform::vector(const Vec3f& v, const Mat& t) {
-		Vec4f V = Vec4f(v.val[0], v.val[1], v.val[2], 0);
+		Vec4f V = Vec4f(v[0], v[1], v[2], 0);
 		V = Vec4f(reinterpret_cast<float*>(Mat(t * Mat(V)).data));
-		return Vec3f(V.val[0], V.val[1], V.val[2]);
+		return Vec3f(V[0], V[1], V[2]);
 	}
 }
