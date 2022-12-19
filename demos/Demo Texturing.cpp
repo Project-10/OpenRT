7afd0cf7f01153e94d2b70a67fafb1d63e847e20
Sergey Kosov <ereator@mail.ru>
2022-12-19 02:08:15 +0100

    Refactor procedural textures

--- demos/Demo Texturing.cpp
+++ demos/Demo Texturing.cpp
@@ -4,36 +4,46 @@
 using namespace rt;
 
 std::shared_ptr<CScene> buildSceneStripes(const Vec3f& bgColor, const Size resolution) {
-	const float		intensity = 5000;
+	
 	auto pScene = std::make_shared<CScene>(bgColor);
 
-	//textures
-	//                                                     (2.0,  6, 0.24, 2.3, 2.5, 0.35 , true)
-	auto pTextureStripes = std::make_shared<CTextureStripes>(5);
+	// Gradient for texture
+	CGradient gradient({{0.0f, RGB(255, 255, 255)}, {0.5f, RGB(255, 255, 255)}, {0.501f, RGB(255, 0, 0)}, {1.0f, RGB(255, 0, 0)}});
+	
+	// Textures
+	auto pTextureStripes = std::make_shared<CTextureStripes>(gradient, 1);
+	//auto pTextureRings = std::make_shared<CTextureRings>(1);
 
-	//Shaders
-	auto pShaderStripes = std::make_shared<CShaderPhong>(*pScene, pTextureStripes, 0.1f, 0.9f, 0.0f, 40.0f);
+	// Shaders
 	auto pShaderFloor = std::make_shared<CShaderPhong>(*pScene, RGB(255, 255, 255), 0.1f, 0.9f, 0.0f, 40.0f);
-
-	//Geometries
+	auto pShaderStripes = std::make_shared<CShaderPhong>(*pScene, pTextureStripes, 0.1f, 0.9f, 2.0f, 320.0f);
+	
+	// Geometries
 	pScene->add(CSolidQuad(pShaderFloor, Vec3f::all(0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
-	CSolid teapot(pShaderStripes, dataPath + "teapot.obj");
+	pScene->add(CSolidBox(pShaderStripes, Vec3f(0, 5, 0), 5));
+	//pScene->add(CSolidSphere(pShaderStripes, Vec3f(0, 5, 0), 5));
+	CSolid teapot(pShaderStripes, dataPath + "Stanford Dragon.obj");
 
-	//Transformation
+	// Transformation
 	CTransform t;
-	teapot.transform(t.scale(5.0f).get());
-	pScene->add(teapot);
+	teapot.transform(t.scale(2.0f).get());
+	//pScene->add(teapot);
 
-	//Light
-	auto pLightPoint = std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, 100, -10));
-	auto pLightSpot = std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(20, 100, -10), Vec3f(0, 0, 0), 15.0f, 30.0f);
-	if (false) pScene->add(pLightPoint);
-	else pScene->add(pLightSpot);
+	// Light
+	const float	intensity = 3e3;
+	const float radius = 66;
+	const float alpha = 8;
+	if (false) {
+		pScene->add(std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, 100, -10)));
+	} else {
+		pScene->add(std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(radius, 100, 0), Vec3f(0, 0, 0), alpha, 2 * alpha));
+		pScene->add(std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(-radius/2, 100, sqrtf(3) * radius/2), Vec3f(0, 0, 0), alpha, 2 * alpha));
+		pScene->add(std::make_shared<CLightSpotTarget>(Vec3f::all(intensity), Vec3f(-radius/2, 100, -sqrtf(3) * radius/2), Vec3f(0, 0, 0), alpha, 2 * alpha));
+	}
 
 
-	//Cameras
-	auto pCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(-15, 30, 40), Vec3f(0, 5, 0), Vec3f(0, 1, 0), 45.0f);
-	pScene->add(pCamera);
+	// Cameras
+	pScene->add(std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(0, 33, 50), Vec3f(0, 5, 0), Vec3f(0, 1, 0), 45.0f));
 
 	return pScene;
 }
@@ -130,7 +140,7 @@ std::shared_ptr<CScene> buildSceneMarble(const Vec3f& bgColor, const Size resolu
 	auto pScene = std::make_shared<CScene>(bgColor);
 
 	// Texture
-	auto pTextureMarble = std::make_shared<CTexture>(dataPath + "marble-light.jpg");
+	// auto pTextureMarble = std::make_shared<CTexture>(dataPath + "marble-light.jpg");
 	CGradient gradientMarble(RGB(166, 208, 229), RGB(175, 152, 123));
 	gradientMarble.addColor(0.37f, RGB(128, 182, 222));
 	gradientMarble.addColor(0.54f, RGB(90, 175, 213));
@@ -146,7 +156,7 @@ std::shared_ptr<CScene> buildSceneMarble(const Vec3f& bgColor, const Size resolu
 	// Shader
 	auto pShaderWhite	= std::make_shared<CShaderFlat>(Vec3f::all(1));
 	auto pShaderFloor	= std::make_shared<CShaderBlinn>(*pScene, RGB(120, 120, 120), 0.1f, 0.9f, 0, 0);
-	auto pShaderMarble	= std::make_shared<CShaderGeneral>(*pScene, pTextureMarble, 0.1f, 0.9f, 0, 0, 0.2f, 0, 1);
+	//auto pShaderMarble	= std::make_shared<CShaderGeneral>(*pScene, pTextureMarble, 0.1f, 0.9f, 0, 0, 0.2f, 0, 1);
 	auto pShaderMarble1	= std::make_shared<CShaderGeneral>(*pScene, pTextureMarble1, 0.1f, 0.9f, 0, 0, 0.2f, 0, 1);
 
 	// Geometry
@@ -172,134 +182,21 @@ std::shared_ptr<CScene> buildSceneMarble(const Vec3f& bgColor, const Size resolu
 
 int main()
 {
-
 	const Vec3f		bgColor = RGB(12, 12, 12);
 	const Size		resolution = Size(800, 600);
-	//	 auto pScene = buildSceneTemplates(bgColor, resolution);
-		 //auto pSceneStripes = buildSceneStripes(bgColor, resolution);
-	  //   auto pSceneRings = buildSceneRings(bgColor, resolution);
+	
+	auto pScene = buildSceneStripes(bgColor, resolution);
+//	auto pScene = buildSceneRings(bgColor, resolution);
+	// auto pScene = buildSceneTemplates(bgColor, resolution);
+	// auto pScene = buildSceneMarble(bgColor, resolution);
 
-	//pScene->buildAccelStructure(25, 3);
-	//Timer::start("Rendering...");
-	//Mat img = pScene->render(std::make_shared<CSamplerStratified>(2, true, true));
-	//Timer::stop();
-	//imshow("Marble Texture", img);
-	//imwrite("D:\\renders\\marble.jpg", img);
-	//waitKey();
-
-
-	const float intensity = 1e4;
-	auto pScene = std::make_shared<CScene>(bgColor);
-
-	// --- Materials ---
-	auto pSHaderWhite = std::make_shared<CShaderFlat>(Vec3f::all(1));
-
-	// Wood shader
-	CGradient gradientWood({ {0.0f, RGB(255, 205, 140)}, {0.1f, RGB(216, 139, 74)}, {0.4f, RGB(226, 147, 82)}, {0.6f, RGB(250, 180, 127)}, {1.0f, RGB(255, 205, 140)} });
-	//CGradient gradientWood({{0.0f, RGB(255, 255, 255)}, {0.499f, RGB(255, 255, 255)}, {0.5f, RGB(255, 0, 0)}, {1.0f, RGB(255, 0, 0)}});
-	auto pTextureWood1 = std::make_shared<CTextureWood>(gradientWood, 2021, 12.0f);
-	auto pTextureWood2 = std::make_shared<CTextureWood>(gradientWood, 2022, 12.0f);
-	auto pShaderWood1 = std::make_shared<CShaderPhong>(*pScene, pTextureWood1, 0.1f, 0.9f, 0.0f, 40.0f);
-	auto pShaderWood2 = std::make_shared<CShaderPhong>(*pScene, pTextureWood2, 0.1f, 0.9f, 0.0f, 40.0f);
-
-	// Marble shader
-	//CGradient gradientMarble(RGB(255, 255, 255), RGB(119, 135, 153));
-	CGradient gradientMarble(Vec3f::all(1), Vec3f::all(0));
-	//gradientMarble.addColor(0.75f, Vec3f::all(0));
-	auto pTextureMarble = std::make_shared<CTextureMarble>(gradientMarble, 0.5f, 7,   6, 1.0f, 16.4f, 2.5f, 0.25f);
-	auto pShaderMarble = std::make_shared<CShaderPhong>(*pScene, pTextureMarble, 0.1f, 0.9f, 0.5f, 20.0f);
-
-	// Stripes shader
-	auto pTextureStripes = std::make_shared<CTextureStripes>(5);
-	auto pShaderStripes = std::make_shared<CShaderPhong>(*pScene, pTextureStripes, 0.1f, 0.9f, 0.5f, 80.0f);
-
-	// Rings shader
-	auto pTextureRings = std::make_shared<CTextureRings>(5);
-	auto pShaderRings = std::make_shared<CShaderPhong>(*pScene, pTextureRings, 0.1f, 0.9f, 0.5f, 80.0f);
-
-
-	// other shaders
-	auto pShaderFloor = std::make_shared<CShaderPhong>(*pScene, RGB(255, 255, 255), 0.5f, 0.5f, 0.0f, 40.0f);
-
-	// Earth shader
-	auto pTextureEarth = std::make_shared<CTexture>(dataPath + "1_earth_8k.jpg");
-	auto pShaderEarth = std::make_shared<CShaderBlinn>(*pScene, pTextureEarth, 0.2f, 0.7f, 0.0f, 40.0f);
-
-
-
-	//Cameras
-	auto pCamera = std::make_shared<CCameraPerspectiveTarget>(resolution, Vec3f(1.5f, 6, 12), Vec3f(0, 0, 0), Vec3f(0, 1, 0), 45.0f);
-	pScene->add(pCamera);
-
-
-	 // Geometries
-	 //pScene->add(CSolidQuad(pShaderFloor, Vec3f(0, -0.52f, 0), Vec3f(0, 1, 0), Vec3f(0, 0, 1), 500));
-
-	 //pScene->add(CSolidBox(pShaderWood, Vec3f(0, 0, 0), 2.5f, 1.0f, 12.0f));
-	 //pScene->add(CSolidBox(pShaderRings, Vec3f(-3, 0, 0), 2.5f, 1.0f, 12.0f));
-
-	 auto sphere1 = CSolid(std::make_shared<CPrimSphere>(pShaderEarth, Vec3f(-4, 1, 0), 1.5f));
-	 auto sphere2 = CSolidSphere(pShaderMarble, Vec3f(0, 1, 0), 1.5f, 64);
-	 //auto sphere2 = CSolidTorus(pShaderWood, Vec3f(0, 1, 0), 1.5f, 1.0f, 24);
-	 auto sphere3 = CSolid(std::make_shared<CPrimSphere>(pShaderMarble, Vec3f(4, 1, 0), 1.5f));
-
-	 // Transform
-	 CTransform T;
-	 Mat rotation = T.rotate(Vec3f(0, 1, 0), 5).get();
-
-	 //sphere1->transform(rotation);
-	 //sphere2.transform(rotation);
-	 //sphere3->transform(rotation);
-
-	 //auto solidSphere = CSolidSphere(pShaderWood, Vec3f(0, 0.5f, 3), 2, 32);
-	 //auto primSphere = std::make_shared<CPrimSphere>(pShaderWood, Vec3f(3, 1, 0), 1.5f);
-	 //primSphere->transform(T.rotate(Vec3f(0, 1, 0), -90).scale(2).get());
-	 //
-	 //pScene->add(std::make_shared<CPrimSphere>(pShaderWood, Vec3f(0, 0, 0), 1.5f));
-	 pScene->add(sphere1);
-	 pScene->add(sphere2);
-	 pScene->add(sphere3);
-
-	 //Light
-	 if (true) {
-		 pScene->add(std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, 100, 50), false));
-		 pScene->add(std::make_shared<CLightOmni>(Vec3f::all(intensity), Vec3f(0, -100, -50), false));
-	 }
-	 else pScene->add(std::make_shared<CLightSky>(Vec3f::all(1), 0.0f));
-
-
-
-	//Mat imgStripes = pSceneStripes->render(std::make_shared<CSamplerStratified>(2, true, true));
-	//imshow("Stripes Texture", imgStripes);
-
-
-     //pSceneRings-> buildAccelStructure(20, 3);
-     //pSceneWood-> buildAccelStructure(20, 3);
-     
-     
-     
-     //Mat imgStripes = pSceneStripes->render(std::make_shared<CSamplerStratified>(2, true, true));
-	 //imshow("Stripes Texture", imgStripes);
+	pScene->buildAccelStructure(20, 3);
+	Timer::start("Rendering...");
+	Mat img = pScene->render(std::make_shared<CSamplerStratified>(1, false, false));
+	Timer::stop();
+	imshow("Image", img);
+	//-imwrite("D:\\renders\\procedural marble.png", img);
+	waitKey();
 	 
-	 //Mat imgRings = pSceneRings->render(std::make_shared<CSamplerStratified>(2, true, true));
-	 //imshow("Rings Texture", imgRings);
-	 
-	 pScene = buildSceneTemplates(bgColor, resolution);
-
-	 for (;;) {
-		 pScene->buildAccelStructure(20, 3);
-		 Timer::start("Rendering...");
-		 Mat img = pScene->render(std::make_shared<CSamplerStratified>(1, true, true));
-		 Timer::stop();
-		 imshow("Image", img);
-		 imwrite("D:\\renders\\procedural marble.png", img);
-		 char key = waitKey();
-		 
-		 sphere1.transform(rotation);
-		 sphere2.transform(rotation);
-		 sphere3.transform(rotation);
-		 
-		 if (key == 27) break;
-	 }
-     return 0;
+	return 0;
 }
--- modules/core/TextureStripes.h
+++ modules/core/TextureStripes.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "Texture.h"
+#include "Gradient.h"
 
 namespace rt {
-	/**
+	class CGradient;
+/**
 	 * @brief Stripes Procedural Texture class
 	 * @note This class is an experimental class
 	 * @ingroup moduleTexture
@@ -14,15 +16,17 @@ namespace rt {
 		/**
 		 * @brief Constructor
 		 * @ingroup moduleTexture
+		 * @param gradient Texture color
 		 * @param period The number of stripes per 1 unit of WCS
 		 */
-		DllExport CTextureStripes(float period) : m_period(period) {}
+		DllExport CTextureStripes(const CGradient& gradient, float period) : m_gradient(gradient), m_period(period) {}
 		DllExport virtual ~CTextureStripes(void) = default;
     
 		DllExport Vec3f	getTexel(const Ray& ray) const override;
 
 
 	private:
-		float m_period;		///< The number of stripes per 1 unit of WCS
+		CGradient	m_gradient;		///< The color gradient for the wood
+		float 		m_period;		///< The number of stripes per 1 unit of WCS
 	};
 }
