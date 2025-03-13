270278501fae7bef3a1d4742a4b20adb7d7ae384
Sergey Kosov <sergey.kosov@project-10.de>
2025-03-13 22:59:32 +0100

    Update ShaderPhong.h

--- modules/core/ShaderPhong.h
+++ modules/core/ShaderPhong.h
@@ -21,7 +21,7 @@ namespace rt {
 		 * @param color The color of the object
 		 * @param ka The ambient coefficient
 		 * @param kd The diffuse reflection coefficient
-		 * @param ks The specular refelection coefficient
+		 * @param ks The specular reflection coefficient
 		 * @param ke The shininess exponent
 		 */
 		DllExport CShaderPhong(const CScene& scene, const Vec3f& color, float ka, float kd, float ks, float ke)
